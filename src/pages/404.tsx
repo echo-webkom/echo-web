e065cbaa76198c04dc1c22ab1a901ff83a612c8a
Andreas Bakseter <andreas_tkd@hotmail.com>
2020-10-29 13:27:54 +0100

    ✨ Forgot types lol

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const NotFoundPage = () => (
+const NotFoundPage = (): JSX.Element => (
     <Layout>
         <SEO title="404: Not found" />
         <h1>404: Not Found</h1>
