8baedf9e59bcf04a717993f960698a917d089ff8
Øyvind Grutle <oyvindgru@gmail.com>
2020-11-03 19:29:20 +0100

    footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Image, Box, SimpleGrid, Heading } from '@chakra-ui/core';
+
+const Footer = (): JSX.Element => {
+    return <Heading as="h3">Halla</Heading>;
+};
+
+export default Footer;
