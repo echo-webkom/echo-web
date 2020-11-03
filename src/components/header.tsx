1e279736bb197caebe32d3f416ed8d20213f0937
Bo Aanes <boaanes@gmail.com>
2020-11-03 18:34:53 +0100

    :sparkles: Replaced bad drawer with chakra drawer
    very nice

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,18 @@
-import React, { useState } from 'react';
-import { Flex, IconButton, Icon, Image, useColorModeValue } from '@chakra-ui/core';
+import React, { useState, useRef } from 'react';
+import {
+    Flex,
+    IconButton,
+    Icon,
+    Image,
+    useColorModeValue,
+    useDisclosure,
+    Drawer,
+    DrawerBody,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+} from '@chakra-ui/core'; // wow very bloat
 
 import NavBar from './navbar';
 
@@ -8,8 +21,9 @@ import imgLogoText from '../assets/echo-logo-very-wide-text-only.png';
 import imgLogoTextWhite from '../assets/echo-logo-very-wide-text-only-white.png';
 
 const Header = (): JSX.Element => {
-    const [show, setShow] = useState(false);
-    const logoText = useColorModeValue(imgLogoText, imgLogoTextWhite);
+    const { isOpen, onOpen, onClose } = useDisclosure(); // state for drawer
+    const menuButtonRef = useRef(null); // ref hook for drawer button
+    const logoText = useColorModeValue(imgLogoText, imgLogoTextWhite); // logo version based on color mode
 
     return (
         <>
@@ -18,7 +32,9 @@ const Header = (): JSX.Element => {
                     <Image src={imgLogo} alt="logo" htmlWidth="30%" />
                     <Image display={{ base: 'none', sm: 'block' }} src={logoText} alt="logo-text" htmlWidth="50%" />
                     <IconButton
-                        onClick={() => setShow((prevShow) => !prevShow)}
+                        variant="unstyled"
+                        ref={menuButtonRef}
+                        onClick={onOpen}
                         display={{ base: 'block', sm: 'none' }}
                         aria-label="show navbar"
                     >
@@ -28,7 +44,21 @@ const Header = (): JSX.Element => {
                     </IconButton>
                 </Flex>
             </Flex>
-            <NavBar visible={show} />
+            <NavBar />
+            <Drawer isOpen={isOpen} placement="right" onClose={onClose} finalFocusRef={menuButtonRef}>
+                <DrawerOverlay>
+                    <DrawerContent>
+                        <DrawerCloseButton />
+                        <DrawerHeader>Navigasjon</DrawerHeader>
+                        <DrawerBody>
+                            <p>hjem</p>
+                            <p>organisasjon</p>
+                            <p>bedrift</p>
+                            <p>om oss</p>
+                        </DrawerBody>
+                    </DrawerContent>
+                </DrawerOverlay>
+            </Drawer>
         </>
     );
 };
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import { Center, Flex } from '@chakra-ui/core';
 
-interface Props {
-    visible: boolean;
-}
-
-const NavBar = ({ visible }: Props): JSX.Element => {
+const NavBar = (): JSX.Element => {
     return (
         <Center>
-            <Flex
-                display={{ base: visible ? 'block' : 'none', sm: 'flex' }}
-                align="center"
-                justify="space-between"
-                w="480px"
-            >
+            <Flex display={{ base: 'none', sm: 'flex' }} align="center" justify="space-between" w="480px">
                 <p>hjem</p>
                 <p>organisasjon</p>
                 <p>bedrift</p>
