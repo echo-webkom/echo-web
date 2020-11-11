3ad93b83e668101da4a20f66195e73ed9ed6b4c4
Bo Aanes <boaanes@gmail.com>
2020-11-11 19:56:11 +0100

    :art: Improved code structure of header/navbar, some color mode things
    added grey palette in theme as well

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Center, IconButton, useColorMode } from '@chakra-ui/core';
-import { VscColorMode } from 'react-icons/vsc';
 
 import Header from './header';
 import Footer from './footer';
@@ -11,26 +9,11 @@ interface Props {
 }
 
 const Layout = ({ children }: Props): JSX.Element => {
-    const { colorMode, toggleColorMode } = useColorMode(); // hook for toggling and using color mode
-
     return (
         <>
-            <IconButton // button for toggling color mode
-                variant="unstyled"
-                icon={
-                    <Center>
-                        <VscColorMode size="2em" />
-                    </Center>
-                }
-                aria-label="toggle color mode"
-                pos="fixed"
-                top="15px"
-                right="15px"
-                onClick={toggleColorMode}
-            />
             <Header />
-            <Footer />
             {children}
+            <Footer />
         </>
     );
 };
