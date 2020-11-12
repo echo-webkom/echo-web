b4233228f25017e09b8442e5b3ff526a199e16ed
Bo Aanes <boaanes@gmail.com>
2020-11-12 22:52:06 +0100

    :sparkles: Added social media links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Box, SimpleGrid, Text, useColorModeValue } from '@chakra-ui/core';
+import { Link, Image, Box, SimpleGrid, Text, useColorModeValue } from '@chakra-ui/core';
 
 import { FiMail } from 'react-icons/fi';
 import { FaFacebook, FaTwitter, FaInstagram, FaGithub } from 'react-icons/fa';
@@ -31,25 +31,48 @@ const Footer = (): JSX.Element => {
             pos="absolute"
         >
             <SimpleGrid columns={2} spacing="3">
-                <Box transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }} color={borderBg}>
-                    <FaFacebook size="4em" color={borderBg} />
-                </Box>
-                <Box transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }} color={borderBg}>
-                    <FaInstagram size="4em" />
-                </Box>
-                <Box transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }} color={borderBg}>
-                    <FaTwitter size="4em" color={borderBg} />
-                </Box>
-                <Box transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }} color={borderBg}>
-                    <FaGithub size="4em" color={borderBg} />
-                </Box>
+                <Link href="https://facebook.com/groups/informatikk" isExternal>
+                    <Box
+                        transition=".1s ease-out"
+                        _hover={{ transform: 'scale(1.05)', cursor: 'pointer' }}
+                        color={borderBg}
+                    >
+                        <FaFacebook size="4em" />
+                    </Box>
+                </Link>
+                <Link href="https://github.com/echo-webkom/echo.uib.no" isExternal>
+                    <Box
+                        transition=".1s ease-out"
+                        _hover={{ transform: 'scale(1.05)', cursor: 'pointer' }}
+                        color={borderBg}
+                    >
+                        <FaGithub size="4em" />
+                    </Box>
+                </Link>
+                <Link href="mailto:echo@uib.no" isExternal>
+                    <Box
+                        transition=".1s ease-out"
+                        _hover={{ transform: 'scale(1.05)', cursor: 'pointer' }}
+                        color={borderBg}
+                    >
+                        <FiMail size="4em" />
+                    </Box>
+                </Link>
+                <Link href="https://instagram.com/echo_uib" isExternal>
+                    <Box
+                        transition=".1s ease-out"
+                        _hover={{ transform: 'scale(1.05)', cursor: 'pointer' }}
+                        color={borderBg}
+                    >
+                        <FaInstagram size="4em" />
+                    </Box>
+                </Link>
             </SimpleGrid>
             <Image display={['none', null, 'block']} htmlWidth="300px" fit="contain" src={logo} />
             <SimpleGrid columns={1} spacing="3" fontSize="lg" maxWidth="170px" textAlign="center">
-                <SimpleGrid columns={2} spacing="2" alignItems="center" color={borderBg}>
-                    <FiMail size="2em" />
+                <Link href="mailto:echo.uib.no">
                     <Text color={borderBg}>echo@uib.no</Text>
-                </SimpleGrid>
+                </Link>
                 <Text color={borderBg}>Thormøhlensgate 55</Text>
                 <Text color={borderBg}>5069 Bergen</Text>
                 <Text color={borderBg}>Org nr: 000 000 000</Text>
