8ed4da41f67c9b6fd321f97f8dfb7238036fcac0
Alvar <alvarhonsi@gmail.com>
2020-11-23 16:29:41 +0100

    :wrench: update jest config, added yarn test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-    moduleNameMapper: {
-        '\\.(scss|sass|css)$': 'identity-obj-proxy',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', 'testing-utils.js', 'testing-wrapper.tsx'],
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     },
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,2 @@
 import '@testing-library/jest-dom';
+import { render } from './src/components/__tests__/testing-utils';
