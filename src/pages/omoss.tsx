efb0cfd93d7f7553888a0092b4f5ca78855bbc4d
Sander Sigmundstad <sanderls@icloud.com>
2021-01-28 17:05:48 +0100

    Removed background color

--- src/pages/omoss.tsx
+++ src/pages/omoss.tsx
@@ -12,43 +12,44 @@ const testPage2 =
 const testPage3 =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus dictum feugiat. Phasellus ut porttitor odio, in euismod orci. Sed lacinia ullamcorper pellentesque. Ut finibus tortor metus, non ultricies massa faucibus id. Donec rutrum ante sed orci feugiat, sit amet porta orci hendrerit. Aenean iaculis rhoncus eros nec fermentum. In tristique scelerisque dui at sagittis. Suspendisse consequat ultricies auctor. Sed odio enim, congue eget cursus dapibus, consectetur eu elit. Quisque vel vulputate nunc. Nullam laoreet feugiat turpis quis molestie. Aenean eget sem ipsum. Aliquam commodo eu justo quis ultricies.';
 
-const OmOssPage = (): JSX.Element => (
-    <Layout>
-        <SEO title="Om Oss" />
-        <Heading size="lg">
-            <Center>Om Oss</Center>
-        </Heading>
-        <Tabs defaultIndex={0} align="center" variant="soft-rounded" variantColor="#231300" orientation="vertical">
-            <TabList margin="20%" marginTop="1%" marginBottom="0">
-                <Tab>Visste du at?</Tab>
-                <Tab>Kontakt Oss</Tab>
-                <Tab>Styret</Tab>
-            </TabList>
-            <TabPanels margin="20%" marginBottom="0" marginTop="0">
-                <TabPanel>
-                    <Container maxW="xl" centerContent>
-                        <Box bgColor="metallicSeaweed.400" padding="4" maxW="3xl">
-                            <Text fontSize="md"> {testPage1} </Text>
-                        </Box>
-                    </Container>
-                </TabPanel>
-                <TabPanel>
-                    <Container maxW="xl" centerContent>
-                        <Box bgColor="metallicSeaweed.400" padding="4" maxW="3xl">
-                            <Text fontSize="md"> {testPage2} </Text>
-                        </Box>
-                    </Container>
-                </TabPanel>
-                <TabPanel>
-                    <Container maxW="xl" centerContent>
-                        <Box bgColor="metallicSeaweed.400" padding="4" maxW="3xl">
-                            <Text fontSize="md"> {testPage3} </Text>
-                        </Box>
-                    </Container>
-                </TabPanel>
-            </TabPanels>
-        </Tabs>
-    </Layout>
-);
-
+const OmOssPage = (): JSX.Element => {
+    return (
+        <Layout>
+            <SEO title="Om Oss" />
+            <Heading size="lg">
+                <Center>Om Oss</Center>
+            </Heading>
+            <Tabs defaultIndex={0} align="center" variant="soft-rounded" variantColor="#231300" orientation="vertical">
+                <TabList margin="20%" marginTop="1%" marginBottom="0">
+                    <Tab>Visste du at?</Tab>
+                    <Tab>Kontakt Oss</Tab>
+                    <Tab>Styret</Tab>
+                </TabList>
+                <TabPanels margin="20%" marginBottom="0" marginTop="0">
+                    <TabPanel>
+                        <Container maxW="xl" centerContent>
+                            <Box padding="4" maxW="3xl">
+                                <Text fontSize="md"> {testPage1} </Text>
+                            </Box>
+                        </Container>
+                    </TabPanel>
+                    <TabPanel>
+                        <Container maxW="xl" centerContent>
+                            <Box padding="4" maxW="3xl">
+                                <Text fontSize="md"> {testPage2} </Text>
+                            </Box>
+                        </Container>
+                    </TabPanel>
+                    <TabPanel>
+                        <Container maxW="xl" centerContent>
+                            <Box padding="4" maxW="3xl">
+                                <Text fontSize="md"> {testPage3} </Text>
+                            </Box>
+                        </Container>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </Layout>
+    );
+};
 export default OmOssPage;
