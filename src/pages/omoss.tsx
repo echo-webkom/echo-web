ae9a8a460148f52e4374175ad4c2c95af284e4d6
Sander Sigmundstad <sanderls@icloud.com>
2021-01-28 17:47:44 +0100

    Added a border to one of the pages too see how it looks. Refactored the code

--- src/pages/omoss.tsx
+++ src/pages/omoss.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-import { Box, Heading, Center, Text, Tabs, TabList, TabPanels, Tab, TabPanel, Square, Circle } from '@chakra-ui/react';
-import { Container } from 'next/app';
+import {
+    Box,
+    Heading,
+    Center,
+    Text,
+    Tabs,
+    TabList,
+    TabPanels,
+    Tab,
+    TabPanel,
+    useColorModeValue,
+} from '@chakra-ui/react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -13,6 +23,7 @@ const testPage3 =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rhoncus dictum feugiat. Phasellus ut porttitor odio, in euismod orci. Sed lacinia ullamcorper pellentesque. Ut finibus tortor metus, non ultricies massa faucibus id. Donec rutrum ante sed orci feugiat, sit amet porta orci hendrerit. Aenean iaculis rhoncus eros nec fermentum. In tristique scelerisque dui at sagittis. Suspendisse consequat ultricies auctor. Sed odio enim, congue eget cursus dapibus, consectetur eu elit. Quisque vel vulputate nunc. Nullam laoreet feugiat turpis quis molestie. Aenean eget sem ipsum. Aliquam commodo eu justo quis ultricies.';
 
 const OmOssPage = (): JSX.Element => {
+    const borderBg = useColorModeValue('mardiGras.400', 'naplesYellow.400');
     return (
         <Layout>
             <SEO title="Om Oss" />
@@ -27,29 +38,24 @@ const OmOssPage = (): JSX.Element => {
                 </TabList>
                 <TabPanels margin="20%" marginBottom="0" marginTop="0">
                     <TabPanel>
-                        <Container maxW="xl" centerContent>
-                            <Box padding="4" maxW="3xl">
-                                <Text fontSize="md"> {testPage1} </Text>
-                            </Box>
-                        </Container>
+                        <Box padding="5" borderWidth="0.1em" border={borderBg}>
+                            <Text fontSize="md"> {testPage1} </Text>
+                        </Box>
                     </TabPanel>
                     <TabPanel>
-                        <Container maxW="xl" centerContent>
-                            <Box padding="4" maxW="3xl">
-                                <Text fontSize="md"> {testPage2} </Text>
-                            </Box>
-                        </Container>
+                        <Box>
+                            <Text fontSize="md"> {testPage2} </Text>
+                        </Box>
                     </TabPanel>
                     <TabPanel>
-                        <Container maxW="xl" centerContent>
-                            <Box padding="4" maxW="3xl">
-                                <Text fontSize="md"> {testPage3} </Text>
-                            </Box>
-                        </Container>
+                        <Box>
+                            <Text fontSize="md"> {testPage3} </Text>
+                        </Box>
                     </TabPanel>
                 </TabPanels>
             </Tabs>
         </Layout>
     );
 };
+
 export default OmOssPage;
