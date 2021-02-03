8138c81a625d6446784d7704ed72dec8bb0e7267
Øyvind Grutle <oyvindgru@gmail.com>
2021-02-03 14:00:08 +0100

    :sparkles: Added post collection page

---
+++ src/components/post-preview.tsx
@@ -0,0 +1,22 @@
+import { Box, Heading, useColorModeValue, Link } from '@chakra-ui/react';
+import Markdown from 'markdown-to-jsx';
+import React from 'react';
+import { Post } from '../lib';
+import MapMarkdownChakra from '../markdown';
+
+const PostPreview = ({ post }: { post: Post }): JSX.Element => {
+    const body = post.body.substring(0, 200);
+    const { slug } = post;
+    const boxBg = useColorModeValue('gray.100', 'gray.900');
+    return (
+        <Box borderWidth="1px" borderRadius="0.75em" overflow="hidden" pl="6" pr="6" bg={boxBg}>
+            <Heading>{post.title}</Heading>
+            <Markdown options={MapMarkdownChakra}>{body}</Markdown>
+            <Link color="purple" href={`posts/${slug}`}>
+                Les mer
+            </Link>
+        </Box>
+    );
+};
+
+export default PostPreview;
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,62 @@
+import { Center, VStack, StackDivider, Text } from '@chakra-ui/react';
+import React from 'react';
+
+import { GetStaticProps } from 'next';
+import PostAPI from '../../lib/api/post';
+import { Author, Post } from '../../lib';
+import PostPreview from '../../components/post-preview';
+import Layout from '../../components/layout';
+
+const PostCollectionPage = ({ posts }: { posts: Array<Post> }): JSX.Element => {
+    return (
+        <Layout>
+            <Center>
+                {posts.length === 0 && <Text>No posts found</Text>}
+                {posts.length !== 0 && (
+                    <VStack divider={<StackDivider borderColor="gray.200" />} spacing={8} align="stretch" width="50%">
+                        {posts.map((post: Post) => {
+                            return <PostPreview post={post} />;
+                        })}
+                    </VStack>
+                )}
+            </Center>
+        </Layout>
+    );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+    try {
+        const { data } = await PostAPI.getPosts(10);
+        return {
+            props: {
+                posts: data.data.postCollection.items.map(
+                    (post: {
+                        title: string;
+                        slug: string;
+                        body: string;
+                        sys: { firstPublishedAt: string };
+                        author: Author;
+                    }) => {
+                        return {
+                            title: post.title,
+                            slug: post.slug,
+                            body: post.body,
+                            publishedAt: post.sys.firstPublishedAt,
+                            author: post.author,
+                        };
+                    },
+                ),
+            },
+            revalidate: 1,
+        };
+    } catch (error) {
+        return {
+            props: {
+                posts: [],
+            },
+            revalidate: 1,
+        };
+    }
+};
+
+export default PostCollectionPage;
