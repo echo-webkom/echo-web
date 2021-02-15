57b8a45a0972ae6492d9b78a1aecd14c2c3b140e
Bo Aanes <boaanes@gmail.com>
2021-02-15 13:49:57 +0100

    :art: API returns formatted types for events

--- src/lib/api/event.ts
+++ src/lib/api/event.ts
@@ -1,4 +1,4 @@
-import { AxiosResponse } from 'axios';
+import { Event, Author } from '..';
 import API from './api';
 
 const GET_PATHS = `
@@ -60,26 +60,97 @@ const GET_EVENT_BY_SLUG = `
 `;
 
 const EventAPI = {
-    getPaths: (): Promise<AxiosResponse> =>
-        API.post('', {
-            query: GET_PATHS,
-        }),
+    getPaths: async (): Promise<Array<string>> => {
+        try {
+            const { data } = await API.post('', {
+                query: GET_PATHS,
+            });
 
-    getEvents: (n: number): Promise<AxiosResponse> =>
-        API.post('', {
-            query: GET_N_EVENTS,
-            variables: {
-                n,
-            },
-        }),
+            return data.data.eventCollection.items.map((event: { slug: string }) => event.slug);
+        } catch (error) {
+            return [];
+        }
+    },
 
-    getEventBySlug: (slug: string): Promise<AxiosResponse> =>
-        API.post('', {
-            query: GET_EVENT_BY_SLUG,
-            variables: {
-                slug,
-            },
-        }),
+    getEvents: async (n: number): Promise<{ events: Array<Event> | null; error: string | null }> => {
+        try {
+            const { data } = await API.post('', {
+                query: GET_N_EVENTS,
+                variables: {
+                    n,
+                },
+            });
+
+            return {
+                events: data.data.eventCollection.items.map(
+                    (event: {
+                        title: string;
+                        slug: string;
+                        date: string;
+                        spots: number;
+                        body: string;
+                        image: {
+                            url: string;
+                        };
+                        location: string;
+                        sys: {
+                            firstPublishedAt: string;
+                        };
+                        author: Author;
+                    }) => {
+                        return {
+                            title: event.title,
+                            slug: event.slug,
+                            date: event.date,
+                            spots: event.spots,
+                            body: event.body,
+                            imageUrl: event.image.url,
+                            location: event.location,
+                            publishedAt: event.sys.firstPublishedAt,
+                            author: event.author,
+                        };
+                    },
+                ),
+                error: null,
+            };
+        } catch (error) {
+            return {
+                events: null,
+                error: `Error retrieveing ${n} events`,
+            };
+        }
+    },
+
+    getEventBySlug: async (slug: string): Promise<{ event: Event | null; error: string | null }> => {
+        try {
+            const { data } = await API.post('', {
+                query: GET_EVENT_BY_SLUG,
+                variables: {
+                    slug,
+                },
+            });
+
+            return {
+                event: {
+                    title: data.data.eventCollection.items[0].title,
+                    slug: data.data.eventCollection.items[0].slug,
+                    date: data.data.eventCollection.items[0].date,
+                    spots: data.data.eventCollection.items[0].spots,
+                    body: data.data.eventCollection.items[0].body,
+                    imageUrl: data.data.eventCollection.items[0].image.url,
+                    location: data.data.eventCollection.items[0].location,
+                    publishedAt: data.data.eventCollection.items[0].sys.firstPublishedAt,
+                    author: data.data.eventCollection.items[0].author,
+                },
+                error: null,
+            };
+        } catch (error) {
+            return {
+                event: null,
+                error: `Event '${slug} not found`,
+            };
+        }
+    },
 };
 
 export default EventAPI;
