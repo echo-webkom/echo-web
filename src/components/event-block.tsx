57657baecf2dacbae825f56b24d905255d435e3b
Andreas Bakseter <andreas_tkd@hotmail.com>
2021-02-28 17:02:52 +0100

    🎨 Event block finished, calendar support postponed
    Calendar view not vital for MVP, moved to its own issue.

---
+++ src/components/event-block.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Box, SimpleGrid, Center, Text, Flex, Image, useColorModeValue } from '@chakra-ui/react';
+import { BiCalendar } from 'react-icons/bi';
+import { ImLocation, ImTicket } from 'react-icons/im';
+import { format } from 'date-fns';
+
+import { Event } from '../lib';
+
+const EventBox = ({ event }: Event): JSX.Element => {
+    const publishedAtStr = format(new Date(event.publishedAt), 'd. MMMM yyyy');
+    const boxBg = useColorModeValue('gray.100', 'gray.900');
+
+    return (
+        <Box borderWidth="1px" borderRadius="0.75em" pl="6" pr="6" bg={boxBg} padding="2em">
+            <SimpleGrid columns={2}>
+                <SimpleGrid rows={2}>
+                    <Flex>
+                        <Text fontSize="4xl" fontWeight="bold">
+                            {event.title}
+                        </Text>
+                    </Flex>
+                    <SimpleGrid rows={3}>
+                        <Flex pt="0.5em">
+                            <BiCalendar size="2em" />
+                            <Text pl="0.5em">{publishedAtStr}</Text>
+                        </Flex>
+                        <Flex pt="0.5em">
+                            <ImLocation size="2em" />
+                            <Text pl="0.5em">{event.location}</Text>
+                        </Flex>
+                        <Flex pt="0.5em">
+                            <ImTicket size="2em" />
+                            {event.spots === 0 ? (
+                                <Text pl="0.5em">Ubegrensede plasser</Text>
+                            ) : (
+                                <Text pl="0.5em">{event.spots} plasser</Text>
+                            )}
+                        </Flex>
+                    </SimpleGrid>
+                </SimpleGrid>
+                <Image width="300px" src={event.imageUrl} alt="Bilde" />
+            </SimpleGrid>
+        </Box>
+    );
+};
+
+const EventBlock = ({ events }: { events: Array<Event> }): JSX.Element => {
+    return (
+        <Center>
+            <Box pt="100px" w="900px" data-testid="events">
+                <Box border="5px">
+                    <SimpleGrid p="50px" spacing={10}>
+                        {events.map((event: Event) => {
+                            return <EventBox event={event} />;
+                        })}
+                    </SimpleGrid>
+                </Box>
+            </Box>
+        </Center>
+    );
+};
+
+export default EventBlock;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
+import { GetStaticProps } from 'next';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import Events from '../components/events';
+import EventBlock from '../components/event-block';
+import { Event } from '../lib';
+import EventAPI from '../lib/api/event';
 
-const IndexPage = (): JSX.Element => (
+const IndexPage = ({ events }: { events: Array<Event> }): JSX.Element => (
     <Layout>
         <SEO title="Home" />
-        <Events />
+        <EventBlock events={events} />
     </Layout>
 );
+
+export const getStaticProps: GetStaticProps = async () => {
+    const { events } = await EventAPI.getEvents(3);
+
+    return {
+        props: {
+            events,
+        },
+    };
+};
 
 export default IndexPage;
