8fb6de92c2d3a947f64f769186af2a25c10e6b36
Bo Aanes <boaanes@gmail.com>
2021-03-08 22:58:27 +0100

    :lipstick: Set min-width and max-width for static-info

--- src/components/static-info.tsx
+++ src/components/static-info.tsx
@@ -15,7 +15,7 @@ const StaticInfo = ({
     return (
         <Tabs orientation="vertical">
             <Grid w="100%" templateColumns={['repeat(1, 1fr)', null, null, 'repeat(4, 1fr)']} gap="4">
-                <GridItem minW="0" colSpan={1}>
+                <GridItem minW="0" maxW="100%" colSpan={1}>
                     <ContentBox>
                         <TabList>
                             {tabNames.map((tabName: string) => (
@@ -24,7 +24,7 @@ const StaticInfo = ({
                         </TabList>
                     </ContentBox>
                 </GridItem>
-                <GridItem colStart={[1, null, null, 2]} colSpan={[1, null, null, 3]} rowSpan={2}>
+                <GridItem minW="0" maxW="100%" colStart={[1, null, null, 2]} colSpan={[1, null, null, 3]} rowSpan={2}>
                     <ContentBox>
                         <TabPanels>
                             {markdownFiles.map((mdFile: string) => (
