ec1c0773feb9e2a8a318f9b988c6db18ae265a77
Bo Aanes <boaanes@gmail.com>
2021-03-08 19:43:27 +0100

    :goal_net: Error handling in post page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,32 +1,41 @@
 import React from 'react';
 
 import { GetStaticProps } from 'next';
+import { Text } from '@chakra-ui/react';
 import { PostAPI } from '../../lib/api';
 import { Post } from '../../lib/types';
 import Layout from '../../components/layout';
 import PostList from '../../components/post-list';
+import ContentBox from '../../components/content-box';
 
-const PostCollectionPage = ({ posts }: { posts: Array<Post> }): JSX.Element => {
+const PostCollectionPage = ({ posts, error }: { posts: Array<Post>; error: string }): JSX.Element => {
     return (
         <Layout>
-            <PostList posts={posts} />
+            {posts && !error && <PostList posts={posts} />}
+            {!posts && error && (
+                <ContentBox>
+                    <Text>{error}</Text>
+                </ContentBox>
+            )}
         </Layout>
     );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-    const { posts } = await PostAPI.getPosts(0); // 0 for all posts
+    const { posts, error } = await PostAPI.getPosts(0); // 0 for all posts
 
     if (posts) {
         return {
             props: {
                 posts,
+                error,
             },
         };
     }
     return {
         props: {
             posts: [],
+            error,
         },
     };
 };
