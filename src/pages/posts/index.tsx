1637cab74cae55052bbd271ada293d5316ae879a
Bo Aanes <boaanes@gmail.com>
2021-03-08 20:06:56 +0100

    :art: Changed error format

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 
 import { GetStaticProps } from 'next';
-import { Text } from '@chakra-ui/react';
 import { PostAPI } from '../../lib/api';
 import { Post } from '../../lib/types';
 import Layout from '../../components/layout';
 import PostList from '../../components/post-list';
-import ContentBox from '../../components/content-box';
 
 const PostCollectionPage = ({ posts, error }: { posts: Array<Post>; error: string }): JSX.Element => {
     return (
         <Layout>
-            {posts && !error && <PostList posts={posts} />}
-            {!posts && error && (
-                <ContentBox>
-                    <Text>{error}</Text>
-                </ContentBox>
-            )}
+            <PostList posts={posts} error={error} />
         </Layout>
     );
 };
