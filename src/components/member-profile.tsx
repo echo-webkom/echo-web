71747c502020b9cb1bccf896a7b0015f36f13dfc
Bo Aanes <boaanes@gmail.com>
2021-04-20 11:57:05 +0200

    :bug: Fixed initials on avatar
    now displays first letters in first and last name

--- src/components/member-profile.tsx
+++ src/components/member-profile.tsx
@@ -5,6 +5,11 @@ import { Profile } from '../lib/api/student-group';
 const MemberProfile = ({ profile, role }: { profile: Profile; role: string }): JSX.Element => {
     const size = useBreakpointValue(['xl', 'xl', '2xl']);
 
+    const getInitials = (name: string) => {
+        const words = name.split(' ');
+        return words.length >= 2 ? `${words[0].charAt(0)}${words[words.length - 1].charAt(0)}` : words[0].charAt(0);
+    };
+
     return (
         <Box
             p={['0', null, '.5em']}
@@ -14,7 +19,13 @@ const MemberProfile = ({ profile, role }: { profile: Profile; role: string }): J
             fontSize={['.7em', '1em']}
         >
             <Center>
-                <Avatar size={size} name={profile.name} src={profile.pictureUrl || undefined} alt="bilde" />
+                <Avatar
+                    getInitials={getInitials}
+                    size={size}
+                    name={profile.name}
+                    src={profile.pictureUrl || undefined}
+                    alt="bilde"
+                />
             </Center>
             <Text my=".5em">{profile.name}</Text>
             <Text as="i">{role}</Text>
