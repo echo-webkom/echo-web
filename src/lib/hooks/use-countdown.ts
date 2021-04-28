b0d181bddaa23c2d2564550c0f85bd041dba4df5
Alvar Hønsi <alvarhonsi@gmail.com>
2021-04-28 20:02:39 +0200

    :sparkles: bedpres now has a timer

--- src/lib/hooks/use-countdown.ts
+++ src/lib/hooks/use-countdown.ts
@@ -1,25 +1,45 @@
 import { useState, useEffect } from 'react';
-import { differenceInMilliseconds, intervalToDuration } from 'date-fns';
+import { differenceInMilliseconds } from 'date-fns';
 
-const useCountdown = (toDate: Date): Duration => {
-    const initialDateInterval = { end: toDate, start: new Date() };
-    const [dateInterval, setDateInterval] = useState(initialDateInterval); // state with current interval
+interface CountdownObject {
+    hours: number;
+    minutes: number;
+    seconds: number;
+}
+
+const formatMs = (ms: number): CountdownObject => {
+    return {
+        hours: ms < 0 ? 0 : Math.floor(ms / (1000 * 3600)),
+        minutes: ms < 0 ? 0 : Math.floor((ms / (1000 * 60)) % 60),
+        seconds: ms < 0 ? 0 : Math.floor((ms / 1000) % 60),
+    };
+};
+
+const useCountdown = (toDate: Date): CountdownObject => {
+    const initialMs = differenceInMilliseconds(toDate, new Date());
+    const [ms, setMs] = useState(initialMs); // state with current interval
 
     useEffect(() => {
         const interval = setInterval(() => {
-            const ms = differenceInMilliseconds(dateInterval.end, dateInterval.start);
             if (ms < 0) {
                 clearInterval(interval); // countdown has reached 0
-            } else {
-                setDateInterval({ end: toDate, start: new Date() });
             }
+            setMs(ms - 1000);
         }, 1000); // Interval repeats every second
+
+        const resync = setInterval(() => {
+            if (ms < 0) {
+                clearInterval(resync);
+            }
+            setMs(differenceInMilliseconds(toDate, new Date()));
+        }, 1000 * 30); // refreshes local datetime every minute
+
         return () => {
             clearInterval(interval);
         };
     });
 
-    return intervalToDuration(dateInterval);
+    return formatMs(ms);
 };
 
 export default useCountdown;
