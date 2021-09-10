b9a202124c453f4cc4a9d7817397dcb4b221b2ce
Bo Aanes <boaanes@gmail.com>
2021-09-10 12:15:35 +0200

    🎨 Added optional colors to IconText

--- src/components/icon-text.tsx
+++ src/components/icon-text.tsx
@@ -1,22 +1,24 @@
-import { Grid, Icon, Link, Text } from '@chakra-ui/react';
+import { ColorProps, Colors, Grid, Icon, Link, Text } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import React from 'react';
 import { IconType } from 'react-icons';
 
 interface Props {
     icon: IconType;
+    iconColor?: ColorProps['color'];
     text: string;
+    textColor?: ColorProps['color'];
     link?: string;
 }
 
-const IconText = ({ icon, text, link }: Props): JSX.Element => {
+const IconText = ({ icon, iconColor, text, textColor, link }: Props): JSX.Element => {
     return (
         <Grid templateColumns="min-content auto" gap="3" wordBreak="break-word" alignItems="center">
-            <Icon as={icon} boxSize={10} />
-            {!link && <Text>{text}</Text>}
+            <Icon as={icon} boxSize={10} color={iconColor} />
+            {!link && <Text color={textColor}>{text}</Text>}
             {link && (
                 <NextLink href={link} passHref>
-                    <Link href={link} isExternal>
+                    <Link href={link} color={textColor} isExternal>
                         {text}
                     </Link>
                 </NextLink>
