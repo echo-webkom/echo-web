9a0ec8930d9a1e60920f54deef4b4ec93b4b7df1
Bo Aanes <boaanes@gmail.com>
2021-10-08 13:52:57 +0200

    👽 Added sanity dataset to env

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client';
 
 export const SanityAPI = sanityClient({
     projectId: 'pgq2pd26',
-    dataset: 'production',
+    dataset: process.env.SANITY_DATASET,
     apiVersion: '2021-04-10',
     token: process.env.SANITY_TOKEN,
     useCdn: true,
