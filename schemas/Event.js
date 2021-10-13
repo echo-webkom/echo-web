77c8bd82b883682833dc15513e40be5fda209c02
Bo Aanes <boaanes@gmail.com>
2021-10-13 19:18:04 +0200

    remove min/max degree year

--- schemas/Event.js
+++ schemas/Event.js
@@ -83,17 +83,5 @@ export default {
                 },
             ],
         },
-        {
-            name: 'minDegreeYear',
-            title: 'Min Degree Year',
-            type: 'number',
-            validation: (Rule) => Rule.required().min(1).max(5),
-        },
-        {
-            name: 'maxDegreeYear',
-            title: 'Max Degree Year',
-            type: 'number',
-            validation: (Rule) => Rule.required().min(Rule.valueOfField('minDegreeYear')).max(5),
-        },
     ],
 };
