1c65d55c1eccfbd4bb549bd79ad554e533f5ac9c
Christian Hein <scheiehein@gmail.com>
2021-10-26 16:35:35 +0200

    scaled down footer, tweaked how it dynamically resizes based on screen width

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -23,90 +23,100 @@ const Footer = (): JSX.Element => {
 
     return (
         <SimpleGrid
-            columns={[1, 2, 3]}
+            columns={[1, 2, 4]}
             bg="bg.dark.secondary"
             justifyItems="center"
             alignItems="center"
             spacingX="3em"
             spacingY="3em"
             w="100%"
-            py="2em"
+            py="0.75em"
             px="1em"
             mt="5em"
             bottom="0"
             pos="absolute"
             data-testid="footer"
         >
-            <SimpleGrid columns={2} spacing="3">
+            <SimpleGrid columns={[4, 4, 2, 4, 4]} spacing="4">
                 <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }} color={color}>
                     <NextLink href="https://facebook.com/groups/informatikk" passHref>
                         <LinkOverlay isExternal>
-                            <Icon as={FaFacebook} boxSize={20} />
+                            <Icon as={FaFacebook} w={12} h={12} />
                         </LinkOverlay>
                     </NextLink>
                 </LinkBox>
                 <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }} color={color}>
                     <NextLink href="https://github.com/echo-webkom/echo.uib.no" passHref>
                         <LinkOverlay isExternal>
-                            <Icon as={FaGithub} boxSize={20} />
+                            <Icon as={FaGithub} w={12} h={12} />
                         </LinkOverlay>
                     </NextLink>
                 </LinkBox>
                 <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }} color={color}>
                     <NextLink href="mailto:echo@uib.no" passHref>
                         <LinkOverlay isExternal>
-                            <Icon as={FiMail} boxSize={20} />
+                            <Icon as={FiMail} w={12} h={12} />
                         </LinkOverlay>
                     </NextLink>
                 </LinkBox>
                 <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }} color={color}>
                     <NextLink href="https://instagram.com/echo_uib" passHref>
                         <LinkOverlay isExternal>
-                            <Icon as={FaInstagram} boxSize={20} />
+                            <Icon as={FaInstagram} w={12} h={12} />
                         </LinkOverlay>
                     </NextLink>
                 </LinkBox>
             </SimpleGrid>
-            <Flex display={['none', null, 'block']}>
-                <Image alt="echo" width={300} height={146} src={echoLogoWhite} />
+            <Flex display={['none', 'block', 'block']}>
+                <Image alt="echo" width={175} height={85} src={echoLogoWhite} />
             </Flex>
-            <SimpleGrid columns={[1, null, null, null, 2]} spacing="10" alignItems="center">
-                <VStack>
-                    <Text color="white">POWERED BY</Text>
+            <SimpleGrid columns={1} alignItems="center">
+                <VStack spacing="0">
+                    <Text fontSize="sm" color="white">
+                        POWERED BY
+                    </Text>
                     <LinkBox>
                         <NextLink href="https://sanity.io" passHref>
                             <LinkOverlay isExternal>
                                 <Center>
-                                    <Image alt="sanity" width={200} height={60} src={sanityLogo} />
+                                    <Image alt="sanity" width={175} height={52.5} src={sanityLogo} />
                                 </Center>
                             </LinkOverlay>
                         </NextLink>
                     </LinkBox>
                 </VStack>
-                <SimpleGrid columns={1} spacing="2" maxWidth="400px" textAlign="center">
-                    <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }}>
-                        <NextLink href="mailto:echo@uib.no" passHref>
-                            <LinkOverlay isExternal>
-                                <Text color={color}>echo(at)uib.no</Text>
-                            </LinkOverlay>
-                        </NextLink>
-                    </LinkBox>
-                    <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }}>
-                        <NextLink href="https://goo.gl/maps/adUsBsoZh3QqNvA36" passHref>
-                            <LinkOverlay isExternal>
-                                <Text color={color}>Thormøhlensgate 55</Text>
-                                <Text color={color}>5006 Bergen</Text>
-                            </LinkOverlay>
-                        </NextLink>
-                    </LinkBox>
-                    <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }}>
-                        <NextLink href="https://w2.brreg.no/enhet/sok/detalj.jsp?orgnr=998995035" passHref>
-                            <LinkOverlay isExternal>
-                                <Text color={color}>Org nr: 998 995 035</Text>
-                            </LinkOverlay>
-                        </NextLink>
-                    </LinkBox>
-                </SimpleGrid>
+            </SimpleGrid>
+            <SimpleGrid columns={1} maxWidth="400px" textAlign="center">
+                <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }}>
+                    <NextLink href="mailto:echo@uib.no" passHref>
+                        <LinkOverlay isExternal>
+                            <Text fontSize="md" color={color}>
+                                echo(at)uib.no
+                            </Text>
+                        </LinkOverlay>
+                    </NextLink>
+                </LinkBox>
+                <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }}>
+                    <NextLink href="https://goo.gl/maps/adUsBsoZh3QqNvA36" passHref>
+                        <LinkOverlay isExternal>
+                            <Text fontSize="md" color={color}>
+                                Thormøhlensgate 55
+                            </Text>
+                            <Text fontSize="md" color={color}>
+                                5006 Bergen
+                            </Text>
+                        </LinkOverlay>
+                    </NextLink>
+                </LinkBox>
+                <LinkBox transition=".1s ease-out" _hover={{ transform: 'scale(1.05)' }}>
+                    <NextLink href="https://w2.brreg.no/enhet/sok/detalj.jsp?orgnr=998995035" passHref>
+                        <LinkOverlay isExternal>
+                            <Text fontSize="md" color={color}>
+                                Org nr: 998 995 035
+                            </Text>
+                        </LinkOverlay>
+                    </NextLink>
+                </LinkBox>
             </SimpleGrid>
         </SimpleGrid>
     );
