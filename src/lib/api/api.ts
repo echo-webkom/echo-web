6c4e87925c2aa875a743833c1c446f221b44d59b
Bo Aanes <boaanes@gmail.com>
2021-10-29 13:56:59 +0200

    👽 Set useCdn to false
    we don't use cdn because we use a private dataset

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -5,5 +5,5 @@ export const SanityAPI = sanityClient({
     dataset: process.env.SANITY_DATASET,
     apiVersion: '2021-04-10',
     token: process.env.SANITY_TOKEN,
-    useCdn: true,
+    useCdn: false,
 });
