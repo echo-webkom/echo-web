5fc990b7bbfd7aac53fe5ad23a8bee21565b966b
Andreas Bakseter <andreas_tkd@hotmail.com>
2021-11-02 12:46:21 +0100

    ✨ Add format for yaml files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,4 +2,5 @@ module.exports = {
     '*.{js,jsx,ts,tsx}': (filenames) =>
         `next lint --fix --file ${filenames.map((file) => file.split(process.cwd())[1]).join(' --file ')}`,
     '*.{js,jsx,ts,tsx,json,md}': 'prettier --write',
+    '*.{yaml,yml}': 'prettier --write --tab-width=2',
 };
