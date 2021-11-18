718ab6021c55464814657af95014f80acdc6d606
nikolaus <Nikolaus.Engh@student.uib.no>
2021-11-18 14:31:28 +0100

    Byttet (at) med @ i footeren

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -78,7 +78,7 @@ const Footer = (): JSX.Element => {
                     <NextLink href="mailto:echo@uib.no" passHref>
                         <LinkOverlay isExternal>
                             <Text fontSize="md" color={color}>
-                                echo(at)uib.no
+                                echo@uib.no
                             </Text>
                         </LinkOverlay>
                     </NextLink>
