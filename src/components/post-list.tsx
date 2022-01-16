69b9701421bb9efb6be9c215a6b09843a7356f3b
Bo Aanes <boaanes@gmail.com>
2022-01-16 13:40:44 +0100

    🎨 Added query parameters for posts
    also some refactoring

--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -1,55 +1,21 @@
-import { Button, Center, Divider, Text, Wrap } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
+import { Center, Wrap } from '@chakra-ui/react';
+import React from 'react';
 import { Post } from '../lib/api';
-import ErrorBox from './error-box';
 import PostPreview from './post-preview';
 
 interface Props {
     posts: Array<Post>;
-    postsPerPage: number;
-    error: string | null;
 }
 
-const PostList = ({ posts, postsPerPage, error }: Props): JSX.Element => {
-    const [index, setIndex] = useState(0);
-
-    useEffect(() => {
-        window.scrollTo(0, window.scrollX);
-    });
-
+const PostList = ({ posts }: Props): JSX.Element => {
     return (
-        <>
-            <Center>
-                {posts.length === 0 && !error && <Text>No posts found</Text>}
-                {posts.length === 0 && error && <ErrorBox error={error} />}
-                {posts.length > 0 && (
-                    <Wrap pt="1rem" className="post-list" spacing={4} w="100%" justify="center">
-                        {posts.slice(index, index + postsPerPage).map((post: Post) => {
-                            return <PostPreview key={post.slug} post={post} data-testid={post.slug} />;
-                        })}
-                    </Wrap>
-                )}
-            </Center>
-            <Divider my="5" />
-            <Center>
-                <Button
-                    mx=".5em"
-                    colorScheme="teal"
-                    display={index !== 0 ? '' : 'none'}
-                    onClick={() => setIndex((prevIndex) => prevIndex - postsPerPage)}
-                >
-                    Forrige
-                </Button>
-                <Button
-                    mx=".5em"
-                    colorScheme="teal"
-                    display={index + postsPerPage < posts.length ? '' : 'none'}
-                    onClick={() => setIndex((prevIndex) => prevIndex + postsPerPage)}
-                >
-                    Neste
-                </Button>
-            </Center>
-        </>
+        <Center>
+            <Wrap pt="1rem" className="post-list" spacing={4} w="100%" justify="center">
+                {posts.map((post: Post) => {
+                    return <PostPreview key={post.slug} post={post} data-testid={post.slug} />;
+                })}
+            </Wrap>
+        </Center>
     );
 };
 
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,9 @@
+import { Center, Divider } from '@chakra-ui/react';
 import { GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 import React from 'react';
+import ButtonLink from '../../components/button-link';
+import ErrorBox from '../../components/error-box';
 import PostList from '../../components/post-list';
 import SEO from '../../components/seo';
 import { Post, PostAPI } from '../../lib/api';
@@ -10,10 +14,30 @@ interface Props {
 }
 
 const PostCollectionPage = ({ posts, error }: Props): JSX.Element => {
+    const router = useRouter();
+    const { page } = router.query;
+    const pageNumber = Number.parseInt(page as string) || 1;
+    const postsPerPage = 6;
+    const slicedPosts = posts.slice((pageNumber - 1) * postsPerPage, (pageNumber - 1) * 6 + postsPerPage);
+
     return (
         <>
             <SEO title="Innlegg" />
-            <PostList posts={posts} postsPerPage={6} error={error} />
+            {error && <ErrorBox error={error} />}
+            {!error && <PostList posts={slicedPosts} />}
+            <Divider my="5" />
+            <Center>
+                {pageNumber !== 1 && (
+                    <ButtonLink linkTo={`posts?page=${pageNumber - 1}`} w="6rem" mr="0.5rem">
+                        Forrige
+                    </ButtonLink>
+                )}
+                {pageNumber * postsPerPage <= posts.length && (
+                    <ButtonLink linkTo={`posts?page=${pageNumber + 1}`} w="6rem" ml="0.5rem">
+                        Neste
+                    </ButtonLink>
+                )}
+            </Center>
         </>
     );
 };
