5e8fac29de48387e53f7a3dc3c0cb1663cdc7755
Ole Magnus Johnsen <ole.magnus@me.com>
2022-02-18 14:23:18 +0100

    :sparkles: Moved tags to _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,8 @@ const CustomDocument = (): JSX.Element => {
                 <meta name="msapplication-TileColor" content="#603cba" />
                 <meta name="theme-color" content={themeColor} />
                 <meta property="og:type" content="website" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
