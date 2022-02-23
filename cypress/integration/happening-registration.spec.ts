6c7066351d8806782da67167674d7697553c768b
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-02-23 18:01:23 +0100

    ✅ Fix & simplify Cypress tests

--- cypress/integration/happening-registration.spec.ts
+++ cypress/integration/happening-registration.spec.ts
@@ -3,6 +3,24 @@
 import users from '../fixtures/users.json';
 import { happenings } from '../fixtures/happening.json';
 
+const checkSubmitRegistration = (degree: string, degreeYear: number) => {
+    cy.get('[data-cy=reg-btn]').click();
+    cy.get('[data-cy=reg-form]').should('be.visible');
+
+    cy.get('input[name=email]').type(`${degree}${degreeYear}@test.com`);
+    cy.get('input[name=firstName]').type('Test');
+    cy.get('input[name=lastName]').type('McTest');
+    cy.get('select[name=degree]').select(degree);
+    cy.get('input[name=degreeYear]').check(degreeYear.toString(), { force: true });
+    cy.get('input[id=terms1]').check({ force: true });
+    cy.get('input[id=terms2]').check({ force: true });
+    cy.get('input[id=terms3]').check({ force: true });
+
+    cy.get('button[type=submit]').click();
+
+    cy.get('li[class=chakra-toast]').contains('Påmeldingen din er registrert!');
+};
+
 describe('Happening registration', () => {
     describe('720p res', () => {
         beforeEach(() => {
@@ -32,9 +50,9 @@ describe('Happening registration', () => {
                     therefore it will never be visible.
 
                     cy.get('input[name=degreeYear]').should('be.visible');
-                    cy.get('input[name=terms1]').should('be.visible');
-                    cy.get('input[name=terms2]').should('be.visible');
-                    cy.get('input[name=terms3]').should('be.visible');
+                    cy.get('input[id=terms1]').should('be.visible');
+                    cy.get('input[id=terms2]').should('be.visible');
+                    cy.get('input[id=terms3]').should('be.visible');
 
                    */
                 });
@@ -42,108 +60,25 @@ describe('Happening registration', () => {
                 for (const b of users.bachelorDegrees) {
                     for (const y of [1, 2, 3]) {
                         it(`User can sign up with valid input (degree = ${b}, degreeYear = ${y}, type = ${type})`, () => {
-                            cy.get('[data-cy=reg-btn]').click();
-                            cy.get('[data-cy=reg-form]').should('be.visible');
-
-                            cy.get('input[name=email]').type(`${b}${y}${users.validUser.email}`);
-                            cy.get('input[name=firstName]').type(users.validUser.firstName);
-                            cy.get('input[name=lastName]').type(users.validUser.lastName);
-                            cy.get('select[name=degree]').select(b);
-                            cy.get('input[name=degreeYear]').check(y.toString(), { force: true });
-                            cy.get('input[name=terms1]').check({ force: true });
-                            cy.get('input[name=terms2]').check({ force: true });
-                            cy.get('input[name=terms3]').check({ force: true });
-
-                            cy.get('button[type=submit]').click();
-
-                            cy.get('li[class=chakra-toast]').contains('Påmeldingen din er registrert!');
+                            checkSubmitRegistration(b, y);
                         });
                     }
                 }
 
-                for (const b of users.masterDegrees) {
+                for (const m of users.masterDegrees) {
                     for (const y of [4, 5]) {
-                        it(`User can sign up with valid input (degree = ${b}, degreeYear = ${y}, type = ${type})`, () => {
-                            cy.get('[data-cy=reg-btn]').click();
-                            cy.get('[data-cy=reg-form]').should('be.visible');
-
-                            cy.get('input[name=email]').type(`${b}${y}${users.validUser.email}`);
-                            cy.get('input[name=firstName]').type(users.validUser.firstName);
-                            cy.get('input[name=lastName]').type(users.validUser.lastName);
-                            cy.get('select[name=degree]').select(b);
-                            cy.get('input[name=degreeYear]').check(y.toString(), { force: true });
-                            cy.get('input[name=terms1]').check({ force: true });
-                            cy.get('input[name=terms2]').check({ force: true });
-                            cy.get('input[name=terms3]').check({ force: true });
-
-                            cy.get('button[type=submit]').click();
-
-                            cy.get('li[class=chakra-toast]').contains('Påmeldingen din er registrert!');
+                        it(`User can sign up with valid input (degree = ${m}, degreeYear = ${y}, type = ${type})`, () => {
+                            checkSubmitRegistration(m, y);
                         });
                     }
                 }
 
                 it(`User can sign up with valid input (degree = ${users.validKogniUser.degree}, degreeYear = ${users.validKogniUser.degreeYear}, type = ${type})`, () => {
-                    cy.get('[data-cy=reg-btn]').click();
-                    cy.get('[data-cy=reg-form]').should('be.visible');
-
-                    cy.get('input[name=email]').type(users.validKogniUser.email);
-                    cy.get('input[name=firstName]').type(users.validKogniUser.firstName);
-                    cy.get('input[name=lastName]').type(users.validKogniUser.lastName);
-                    cy.get('select[name=degree]').select(users.validKogniUser.degree);
-                    cy.get('input[name=degreeYear]').check(users.validKogniUser.degreeYear.toString(), { force: true });
-                    cy.get('input[name=terms1]').check({ force: true });
-                    cy.get('input[name=terms2]').check({ force: true });
-                    cy.get('input[name=terms3]').check({ force: true });
-
-                    cy.get('button[type=submit]').click();
-
-                    cy.get('li[class=chakra-toast]').contains('Påmeldingen din er registrert!');
+                    checkSubmitRegistration(users.validKogniUser.degree, users.validKogniUser.degreeYear);
                 });
 
                 it(`User can sign up with valid input (degree = ${users.validArmninfUser.degree}, degreeYear = ${users.validArmninfUser.degreeYear}, type = ${type})`, () => {
-                    cy.get('[data-cy=reg-btn]').click();
-                    cy.get('[data-cy=reg-form]').should('be.visible');
-
-                    cy.get('input[name=email]').type(users.validArmninfUser.email);
-                    cy.get('input[name=firstName]').type(users.validArmninfUser.firstName);
-                    cy.get('input[name=lastName]').type(users.validArmninfUser.lastName);
-                    cy.get('select[name=degree]').select(users.validArmninfUser.degree);
-                    cy.get('input[name=degreeYear]').check(users.validArmninfUser.degreeYear.toString(), {
-                        force: true,
-                    });
-                    cy.get('input[name=terms1]').check({ force: true });
-                    cy.get('input[name=terms2]').check({ force: true });
-                    cy.get('input[name=terms3]').check({ force: true });
-
-                    cy.get('button[type=submit]').click();
-
-                    cy.get('li[class=chakra-toast]').contains('Påmeldingen din er registrert!');
-                });
-
-                it("User can't sign up if not all terms are accepted", () => {
-                    cy.get('[data-cy=reg-btn]').click();
-                    cy.get('[data-cy=reg-form]').should('be.visible');
-
-                    cy.get('input[name=email]').type(users.validUser.email);
-                    cy.get('input[name=firstName]').type(users.validUser.firstName);
-                    cy.get('input[name=lastName]').type(users.validUser.lastName);
-                    cy.get('select[name=degree]').select(users.validUser.degree);
-                    cy.get('input[name=degreeYear]').check(users.validUser.degreeYear.toString(), { force: true });
-                    cy.get('input[name=terms1]').check({ force: true });
-                    cy.get('input[name=terms3]').check({ force: true });
-
-                    cy.get('button[type=submit]').click();
-
-                    if (type === 'BEDPRES') {
-                        cy.get('li[class=chakra-toast]').contains('Du må godkjenne Bedkom sine retningslinjer.');
-                    } else if (type === 'EVENT') {
-                        cy.get('li[class=chakra-toast]').contains('Du må godkjenne vilkårene.');
-                    } else {
-                        throw new Error(`'type' is not 'BEDPRES' or 'EVENT', but '${type}'.`);
-                    }
-
-                    cy.get('li[class=chakra-toast]').contains('Vennligst prøv igjen.');
+                    checkSubmitRegistration(users.validArmninfUser.degree, users.validArmninfUser.degreeYear);
                 });
             });
         }
