9a7f9e46c01a116c0c05428d6bb66a5f8370233f
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-04-24 12:00:59 +0200

    ✅ Fix Jest

--- src/components/__tests__/testing-wrapper.tsx
+++ src/components/__tests__/testing-wrapper.tsx
@@ -1,9 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { SessionProvider } from 'next-auth/react';
 import React, { FunctionComponent } from 'react';
 import theme from '../../styles/theme';
 
 const AllTheProviders: FunctionComponent = ({ children }) => {
-    return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+    return (
+        <SessionProvider
+            session={{
+                expires: new Date(Date.now() + 2 * 86400).toISOString(),
+                user: { name: 'admin' },
+            }}
+        >
+            <ChakraProvider theme={theme}>{children}</ChakraProvider>
+        </SessionProvider>
+    );
 };
 
 export default AllTheProviders;
