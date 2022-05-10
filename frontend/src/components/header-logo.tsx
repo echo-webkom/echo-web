b3a59a3495c1f2aa1f73efffa359e22c7ec300bc
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-05-10 17:10:54 +0200

    ♻️ Change priority of msg

--- frontend/src/components/header-logo.tsx
+++ frontend/src/components/header-logo.tsx
@@ -35,14 +35,14 @@ const randomHeaderMessage = (): string => {
 
     if (getMonth(now) === 4 && getDate(now) === 17) {
         return 'Gralla 🇳🇴';
-    } else if (isMonday(now)) {
-        return 'New week, new me?';
     } else if (isThursday(now) && getHours(now) < 12) {
         return 'Husk bedpres kl. 12:00!';
     } else if (getMonth(now) === 11 && getDate(now) >= 24) {
         return 'God jul! 🎅';
     } else if (getMonth(now) === 0 && getDate(now) === 1) {
         return 'Godt nyttår! ✨';
+    } else if (isMonday(now)) {
+        return 'New week, new me?';
     }
 
     return stdMessages()[Math.floor(Math.random() * stdMessages().length)];
