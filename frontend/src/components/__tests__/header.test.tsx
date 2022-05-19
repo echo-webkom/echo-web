2abfb124aa19ce05d3cc9ff948435e7e846bd13f
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-05-19 16:39:15 +0200

    ✅ Fix header test

--- frontend/src/components/__tests__/header.test.tsx
+++ frontend/src/components/__tests__/header.test.tsx
@@ -1,29 +1,50 @@
 import userEvent from '@testing-library/user-event';
 import React from 'react';
+import { screen, waitFor } from '@testing-library/react';
 import Header from '../header';
 import { render } from './testing-utils';
 
 describe('Header', () => {
-    test('renders without crashing', () => {
-        const { getByTestId } = render(<Header />);
-        expect(getByTestId(/header-standard/i)).toBeInTheDocument();
+    test('renders without crashing', async () => {
+        render(<Header />);
+        const header = screen.getByTestId(/header-standard/i);
+
+        await waitFor(() => header);
+
+        expect(header).toBeInTheDocument();
     });
 
-    test('renders correctly', () => {
-        const { getByTestId } = render(<Header />);
-        expect(getByTestId(/header-standard/i)).toBeInTheDocument();
-        expect(getByTestId(/header-logo/i)).toBeInTheDocument();
-        expect(getByTestId(/navbar-standard/i)).toBeInTheDocument();
-        expect(getByTestId(/drawer-button/i)).toBeInTheDocument();
+    test('renders correctly', async () => {
+        render(<Header />);
+        const header = screen.getByTestId(/header-standard/i);
+        const headerLogo = screen.getByTestId(/header-logo/i);
+        const navbarStandard = screen.getByTestId(/navbar-standard/i);
+        const drawerButton = screen.getByTestId(/drawer-button/i);
+
+        await waitFor(() => header);
+        await waitFor(() => headerLogo);
+        await waitFor(() => navbarStandard);
+        await waitFor(() => drawerButton);
+
+        expect(header).toBeInTheDocument();
+        expect(headerLogo).toBeInTheDocument();
+        expect(navbarStandard).toBeInTheDocument();
+        expect(drawerButton).toBeInTheDocument();
     });
 
-    test('drawer button opens a chakra drawer', () => {
-        const { getByTestId, getByText } = render(<Header />);
-        const drawerButton = getByTestId(/drawer-button/i);
-        userEvent.click(drawerButton);
-        // drawer exists in DOM
-        expect(getByTestId(/navbar-drawer/i)).toBeInTheDocument();
-        // Drawer Header exists
-        expect(getByText(/navigasjon/i)).toBeInTheDocument();
+    test('drawer button opens a chakra drawer', async () => {
+        const user = userEvent.setup();
+        render(<Header />);
+        const drawerButton = screen.getByTestId(/drawer-button/i);
+
+        await waitFor(() => drawerButton);
+        expect(drawerButton).toBeInTheDocument();
+
+        await user.click(drawerButton);
+
+        const navText = screen.getByText(/navigasjon/i);
+        await waitFor(() => navText);
+
+        expect(navText).toBeInTheDocument();
     });
 });
