b86e045a207838b8e113d5836f5e46c118103b3e
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-06-07 22:02:49 +0200

    🔧 Make info for studentgroups not required
    Relevant for hovedstyrer

--- frontend/src/pages/om-echo/studentgrupper/[slug].tsx
+++ frontend/src/pages/om-echo/studentgrupper/[slug].tsx
@@ -32,19 +32,26 @@ const StudentGroupPage = ({ studentGroup }: Props): JSX.Element => {
                             {studentGroup.name}
                         </Heading>
                         <Divider mb="1rem" />
-                        <Markdown options={{ overrides: MapMarkdownChakra }}>{studentGroup.info}</Markdown>
-                        {studentGroup.imageUrl && (
-                            <Center>
-                                <Image
-                                    src={studentGroup.imageUrl}
-                                    alt=""
-                                    objectFit="cover"
-                                    maxHeight="570px"
-                                    minWidth="100%"
-                                />
-                            </Center>
+                        {studentGroup.info && (
+                            <>
+                                <Markdown options={{ overrides: MapMarkdownChakra }}>{studentGroup.info}</Markdown>
+                                <Divider my="5" />
+                            </>
                         )}
-                        <Divider my="5" />
+                        {studentGroup.imageUrl && (
+                            <>
+                                <Center>
+                                    <Image
+                                        src={studentGroup.imageUrl}
+                                        alt=""
+                                        objectFit="cover"
+                                        maxHeight="570px"
+                                        minWidth="100%"
+                                    />
+                                </Center>
+                                <Divider my="5" />
+                            </>
+                        )}
                         <Wrap spacing={['1em', null, '2.5em']} justify="center">
                             {studentGroup.members.map((member: Member) => (
                                 <WrapItem key={member.profile.name}>
