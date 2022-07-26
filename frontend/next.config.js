30638329378b2fc3ac52887df9201cfc02b869ae
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-07-26 18:07:45 +0200

    🔧 Fix invalid next.config.js
    https://github.com/shadowwalker/next-pwa/issues/367#issuecomment-1194057483

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 const withPWA = require('next-pwa');
 
-module.exports = withPWA({
+const pwaConfig = withPWA({
     pwa: {
         dest: 'public',
         disable: process.env.NODE_ENV === 'development',
@@ -31,3 +31,7 @@ module.exports = withPWA({
     },
     reactStrictMode: true,
 });
+
+delete pwaConfig.pwa;
+
+module.exports = pwaConfig;
