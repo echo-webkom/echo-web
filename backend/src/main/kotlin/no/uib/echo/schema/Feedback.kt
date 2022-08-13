59798f778bf6b9ebcf8fb1af4d2618937da066f0
Ole Magnus Johnsen <ole.magnus@me.com>
2022-08-13 16:57:51 +0200

    :bug: Use datetime

--- backend/src/main/kotlin/no/uib/echo/schema/Feedback.kt
+++ backend/src/main/kotlin/no/uib/echo/schema/Feedback.kt
@@ -3,12 +3,15 @@ package no.uib.echo.schema
 import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.jodatime.CurrentDateTime
+import org.jetbrains.exposed.sql.jodatime.datetime
+import org.joda.time.DateTime
 
 @Serializable
 data class FeedbackJson(val email: String?, val name: String?, val message: String)
 
 @Serializable
-data class FeedbackResponseJson(val email: String?, val name: String?, val message: String, val sent: String)
+data class FeedbackResponseJson(val email: String?, val name: String?, val message: String, val sent: String? = null)
 
 enum class FeedbackResponse {
     EMPTY,
@@ -20,7 +23,7 @@ object Feedback : Table() {
     val email: Column<String?> = text("email").nullable()
     val name: Column<String?> = text("name").nullable()
     val message: Column<String> = text("message")
-    val sent: Column<String> = text("sent")
+    val sent: Column<DateTime> = datetime("sent").defaultExpression(CurrentDateTime)
 
     override val primaryKey: PrimaryKey = PrimaryKey(id)
 }
