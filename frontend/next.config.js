f1d464c8bc5d853e0aab3323f6476a820aed2b21
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-08-24 22:21:18 +0200

    🔧 Change to new config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,9 @@
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+});
 
 module.exports = withPWA({
-    pwa: {
-        dest: 'public',
-        disable: process.env.NODE_ENV === 'development',
-    },
     webpack: (config) => {
         config.module.rules.push({
             test: /\.md$/,
