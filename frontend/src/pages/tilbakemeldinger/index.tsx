aaafaf9244c819ac0204d3aac0ca7161e1183af8
Ole Magnus Fon Johnsen <ole.magnus@me.com>
2022-08-29 13:45:33 +0200

    🚧 Display feedbacks

--- frontend/src/pages/tilbakemeldinger/index.tsx
+++ frontend/src/pages/tilbakemeldinger/index.tsx
@@ -1,58 +1,107 @@
-import { Heading, Box, Text } from '@chakra-ui/react';
-import { GetServerSideProps } from 'next';
+import { Heading, Box, Text, SimpleGrid, GridItem, Divider, Center } from '@chakra-ui/react';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 import Section from '../../components/section';
 import SEO from '../../components/seo';
 import { UserAPI } from '../../lib/api';
 import { FeedbackAPI } from '../../lib/api/feedback';
-import { Feedback, isErrorMessage } from '../../lib/api/types';
+import { ErrorMessage, Feedback, isErrorMessage, UserWithName } from '../../lib/api/types';
 
 interface Props {
-    feedbacks: Array<Feedback>;
+    backendUrl: string;
+    adminKey: string;
 }
 
-const FeedbackPage = ({ feedbacks }: Props) => {
+const FeedbackPage = ({ backendUrl, adminKey }: Props) => {
+    const [feedbacks, setFeedbacks] = useState<Array<Feedback>>([]);
+    const [user, setUser] = useState<UserWithName | null>();
+    const [error, setError] = useState<ErrorMessage | null>(null);
+    const gridColumns = [1, null, 2, null, 3];
+
+    useEffect(() => {
+        const fetchFeedbacks = async () => {
+            const feedbacks = await FeedbackAPI.getFeedback(backendUrl, adminKey, user.email);
+            if (isErrorMessage(feedbacks)) {
+                setError(feedbacks);
+            } else {
+                setFeedbacks(feedbacks);
+            }
+        };
+        if (user) void fetchFeedbacks();
+    }, [backendUrl, adminKey, user]);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const user = await UserAPI.getUser();
+            if (isErrorMessage(user)) {
+                setError(user);
+            } else {
+                setUser(user);
+            }
+        };
+        void fetchUser();
+    });
+
     return (
         <>
             <SEO title="Tilbakemeldinger" />
             <Section>
-                <Heading>Tilbakemeldinger</Heading>
-                {feedbacks.map((feedback, i) => (
-                    <Box key={i} bg="lightgray" borderRadius="0.25rem">
-                        <Text>Navn: {feedback.name}</Text>
-                        <Text>E-post: {feedback.email}</Text>
-                        <Text>Tilbakemelding:{feedback.message}</Text>
-                    </Box>
-                ))}
+                {error && (
+                    <Center>
+                        <Heading>{error.message}</Heading>
+                    </Center>
+                )}
+                {!error && (
+                    <>
+                        <Heading>Tilbakemeldinger</Heading>
+                        <Text fontWeight="bold" decoration="underline">
+                            Antall tilbakemeldinger: {feedbacks.length}
+                        </Text>
+                        <Divider my="2" />
+                        <SimpleGrid columns={gridColumns}>
+                            {feedbacks.length > 0 ? (
+                                feedbacks.map((feedback, i) => {
+                                    const formattedDate = (date: Date) => {
+                                        const year = date.getFullYear();
+                                        const month = date.getMonth() + 1;
+                                        const day = date.getDate();
+                                        const hour = date.getHours();
+                                        const minute = date.getMinutes();
+
+                                        return `${day}-${month}-${year} ${hour}:${minute}`;
+                                    };
+
+                                    return (
+                                        <Box key={i} p={4} shadow="md" borderWidth="1px">
+                                            <Text fontWeight="bold" decoration="underline">
+                                                {feedback.name ?? 'Anonym'}
+                                            </Text>
+                                            <Text>{feedback.email ?? 'Anonym'}</Text>
+                                            <Text>{feedback.message}</Text>
+                                            <Text>{formattedDate(new Date(feedback.sent))}</Text>
+                                        </Box>
+                                    );
+                                })
+                            ) : (
+                                <GridItem colSpan={gridColumns}>Ingen tilbakemeldinger</GridItem>
+                            )}
+                        </SimpleGrid>
+                    </>
+                )}
             </Section>
         </>
     );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps = () => {
     const backendUrl = process.env.NEXT_PUBLIC_BACEND_URL ?? 'http://localhost:8080';
     const adminKey = process.env.ADMIN_KEY;
     if (!adminKey) throw new Error('No ADMIN_KEY defined.');
 
-    const user = await UserAPI.getUser();
-
-    if (user && !isErrorMessage(user)) {
-        const feedbacks = await FeedbackAPI.getFeedback(backendUrl, adminKey, user.email);
-
-        if (!isErrorMessage(feedbacks)) {
-            return {
-                props: {
-                    feedbacks,
-                },
-            };
-        }
-    }
-
     return {
         props: {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
+            backendUrl,
+            adminKey,
         },
     };
 };
