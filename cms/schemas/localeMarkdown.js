3b15e13642fd7dcfb4c9960fb5fe13389f1f2d06
felix <mrKaasa@protonmail.com>
2022-08-30 14:47:44 +0200

    removed a validation check. squash this with the last commit

--- cms/schemas/localeMarkdown.js
+++ cms/schemas/localeMarkdown.js
@@ -3,7 +3,6 @@ import languages from '../languages';
 export default {
     name: 'localeMarkdown',
     title: 'localeMarkdown',
-    validation: (Rule) => Rule.required(),
     type: 'object',
     fields: languages.map((lang) => ({
         title: lang.title,
