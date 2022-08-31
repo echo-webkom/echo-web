d790e4357a72a33b652515331e627a675e414963
Ole Magnus Fon Johnsen <ole.magnus@me.com>
2022-08-31 12:19:26 +0200

    💄 Move language menu

--- frontend/src/components/article.tsx
+++ frontend/src/components/article.tsx
@@ -1,7 +1,7 @@
-import { Divider, Heading } from '@chakra-ui/react';
+import { Divider, Flex, Heading, Spacer } from '@chakra-ui/react';
 import Markdown from 'markdown-to-jsx';
-import React from 'react';
 import MapMarkdownChakra from '../markdown';
+import LanguageMenu from './language-menu';
 
 interface Props {
     heading: string;
@@ -11,9 +11,13 @@ interface Props {
 const Article = ({ heading, body }: Props): JSX.Element => {
     return (
         <>
-            <Heading mb="0.2em" size="2xl">
-                {heading}
-            </Heading>
+            <Flex direction="row" alignItems="center">
+                <Heading mb="0.2em" size="2xl">
+                    {heading}
+                </Heading>
+                <Spacer />
+                <LanguageMenu />
+            </Flex>
             <Divider my=".5em" />
             <Markdown options={{ overrides: MapMarkdownChakra }}>{body}</Markdown>
         </>
