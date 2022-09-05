5739764c613ce26e0f810f3c03c92c2175fa4a7f
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-09-05 19:11:36 +0200

    🎨 ktlint format

--- backend/src/main/kotlin/no/uib/echo/DatabaseHandler.kt
+++ backend/src/main/kotlin/no/uib/echo/DatabaseHandler.kt
@@ -7,14 +7,14 @@ import no.uib.echo.schema.Feedback
 import no.uib.echo.schema.Happening
 import no.uib.echo.schema.Registration
 import no.uib.echo.schema.SpotRange
+import no.uib.echo.schema.StudentGroup
+import no.uib.echo.schema.StudentGroupMembership
 import no.uib.echo.schema.User
 import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.net.URI
-import no.uib.echo.schema.StudentGroup
-import no.uib.echo.schema.StudentGroupMembership
 
 private const val DEFAULT_DEV_POOL_SIZE = 10
 private const val DEFAULT_PROD_POOL_SIZE = 50
