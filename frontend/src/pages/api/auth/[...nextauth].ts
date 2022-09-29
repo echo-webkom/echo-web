054e319082d7d55c620e3c8a594a038e1b20ac20
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-09-29 15:56:54 +0200

    🔧 Increase JWT expire to 1 week
    Da er man logget inn i én uke uten å måtte logge inn på nytt.

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -3,10 +3,10 @@ import type { NextAuthOptions } from 'next-auth';
 
 export const authOptions: NextAuthOptions = {
     session: {
-        maxAge: 3600,
+        maxAge: 3600 * 24 * 7, // 7 days
     },
     jwt: {
-        maxAge: 3600,
+        maxAge: 3600 * 24 * 7, // 7 days
     },
     callbacks: {
         // eslint-disable-next-line @typescript-eslint/require-await
