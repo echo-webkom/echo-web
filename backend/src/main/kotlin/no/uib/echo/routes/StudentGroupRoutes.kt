b078cccf5237f9f6bfe252a32f881a540b8d577a
Ole Magnus Fon Johnsen <ole.magnus@me.com>
2022-10-24 07:33:27 +0200

    remove redudant endpoint

--- backend/src/main/kotlin/no/uib/echo/routes/StudentGroupRoutes.kt
+++ backend/src/main/kotlin/no/uib/echo/routes/StudentGroupRoutes.kt
@@ -15,70 +15,25 @@ import io.ktor.server.routing.routing
 import kotlinx.serialization.Serializable
 import no.uib.echo.schema.StudentGroupMembership
 import no.uib.echo.schema.getGroupMembers
-import no.uib.echo.schema.validStudentGroups
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.StdOutSqlLogger
 import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.studentGroupRoutes() {
-    routing {
-        authenticate("auth-jwt") {
-            getIsInGroup()
-            putMembership()
-        }
-    }
+    routing { authenticate("auth-jwt") { putMembership() } }
 }
 
-/**
- * Returns OK if user is in group, otherwise NotFound.
- */
-fun Route.getIsInGroup() {
-    validStudentGroups.map { group ->
-        get("/$group") {
-            val email = call.principal<JWTPrincipal>()?.payload?.getClaim("email")?.asString()?.lowercase()
-
-            if (email == null) {
-                call.respond(HttpStatusCode.Unauthorized)
-                return@get
-            }
-
-            if (email !in getGroupMembers("webkom")) {
-                call.respond(HttpStatusCode.Forbidden)
-                return@get
-            }
-
-            val isMember = transaction {
-                addLogger(StdOutSqlLogger)
-
-                StudentGroupMembership.select {
-                    StudentGroupMembership.userEmail eq email
-                    StudentGroupMembership.studentGroupName eq group
-                }.firstOrNull() != null
-            }
-
-            if (isMember) {
-                call.respond(HttpStatusCode.OK)
-            } else {
-                call.respond(HttpStatusCode.NotFound)
-            }
-        }
-    }
-}
-
-/**
- * Removes previous memberships and adds new ones of user.
- */
 fun Route.putMembership() {
     put("/membership") {
         @Serializable
         data class RequestJson(val userEmail: String, val studentGroups: List<String>)
 
         val req = call.receive<RequestJson>()
-        val email = call.principal<JWTPrincipal>()?.payload?.getClaim("email")?.asString()?.lowercase()
+        val email =
+                call.principal<JWTPrincipal>()?.payload?.getClaim("email")?.asString()?.lowercase()
 
         if (email == null) {
             call.respond(HttpStatusCode.Unauthorized)
@@ -93,9 +48,7 @@ fun Route.putMembership() {
         transaction {
             addLogger(StdOutSqlLogger)
 
-            StudentGroupMembership.deleteWhere {
-                StudentGroupMembership.userEmail eq req.userEmail
-            }
+            StudentGroupMembership.deleteWhere { StudentGroupMembership.userEmail eq req.userEmail }
         }
 
         transaction {
@@ -109,6 +62,9 @@ fun Route.putMembership() {
             }
         }
 
-        call.respond(HttpStatusCode.OK, "User ${req.userEmail} is now a member of ${req.studentGroups}")
+        call.respond(
+                HttpStatusCode.OK,
+                "User ${req.userEmail} is now a member of ${req.studentGroups}"
+        )
     }
 }
