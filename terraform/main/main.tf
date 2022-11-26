8d1c692d5fa6999e032b3bcdcabd821524210f8b
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-11-26 16:22:12 +0100

    🏗 Add Terraform config for production environment

---
+++ terraform/main/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.34.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "echo-web-tfstate"
+    storage_account_name = "echowebtfstatestore"
+    container_name       = "tfstate"
+    key                  = "azure.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = "f16e6916-1e71-42a0-9df3-0246b805f432"
+}
+
+locals {
+  location = "norwayeast"
+}
+
+module "prod" {
+  source = "./prod"
+
+  location    = var.location
+  cg_name     = "echo-web-prod"
+  db_password = var.db_password_prod
+  admin_key   = var.admin_key_prod
+}
+
+module "dev" {
+  source = "./dev"
+
+  location    = var.location
+  cg_name     = "echo-web-dev"
+  db_password = var.db_password_dev
+  admin_key   = var.admin_key_dev
+  auth_secret = var.auth_secret
+}
---
+++ terraform/main/modules/postgres/outputs.tf
@@ -0,0 +1,3 @@
+output "fqdn" {
+  value = azurerm_postgresql_server.db.fqdn
+}
