fe32c9e731817b5a7124a9e9f60cbde3ba23b522
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-11-27 20:27:22 +0100

    ♻️  Change to correct text on /nei & simplify logic
    Fjernet noen tilfeller som aldri har skjedd hittil (i følge
    tilbakemeldinger). Nå logger vi bare om noen ikke er representert av
    echo.

--- frontend/src/pages/nei.tsx
+++ frontend/src/pages/nei.tsx
@@ -1,16 +1,23 @@
 import { Box, Center, Heading, Text } from '@chakra-ui/react';
+import useLanguage from '@hooks/use-language';
 
-const NeiPage = (): JSX.Element => (
-    <Center>
-        <Box textAlign="center">
-            <Heading py="3rem" fontSize="8rem">
-                Nei
-            </Heading>
-            <Text px="25%" fontSize="2rem">
-                Du er ikke medlem av institutt for informatikk. Om du mener dette ikke stemmer, ta kontakt med Webkom.
-            </Text>
-        </Box>
-    </Center>
-);
+const NeiPage = () => {
+    const isNorwegian = useLanguage();
+
+    return (
+        <Center>
+            <Box textAlign="center">
+                <Heading py="3rem" fontSize="8rem">
+                    {isNorwegian ? 'Nei' : 'No'}
+                </Heading>
+                <Text px="20%" fontSize="1.5rem">
+                    {isNorwegian
+                        ? 'I følge echo sine vedtekter er du ikke representert av echo. Om du mener dette ikke stemmer, ta kontakt med Webkom, f.eks. via tilbakemeldingsskjemaet nederst i høyre hjørne eller på mail webkom-styret@echo.uib.no.'
+                        : 'According to echos bylaws, you are not represented by echo. If you believe this is incorrect, please contact Webkom, for example via the feedback form in the bottom right corner or by email webkom-styret@echo.uib.no.'}
+                </Text>
+            </Box>
+        </Center>
+    );
+};
 
 export default NeiPage;
