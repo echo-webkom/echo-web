4e40c40dd9a2c8b98f76e6c0dea3692a7c5cf2c3
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-12-02 00:32:59 +0100

    ✅ Update Cypress

--- frontend/cypress/e2e/happening-registration.spec.ts
+++ frontend/cypress/e2e/happening-registration.spec.ts
@@ -1,33 +1,25 @@
-import { feide } from '../fixtures/feide.json';
+import { email, name, password } from '../fixtures/user.json';
 import { happenings } from '../fixtures/happening.json';
 import signIn from '../support/e2e';
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from failing the test
-    return false;
-});
-
 describe('Happening registration', () => {
     describe('720p res', () => {
-        for (const { username, password, email, name } of feide) {
-            beforeEach(() => {
-                cy.viewport(1280, 720);
-            });
+        beforeEach(() => {
+            cy.viewport(1280, 720);
+            signIn(email, name, password);
+        });
 
-            for (const { slug } of happenings) {
-                describe('Happening form registration', () => {
-                    it('should register user to happening', () => {
-                        signIn(username, password, email, name);
+        for (const { slug } of happenings) {
+            describe('Happening form registration', () => {
+                it('should register user to happening', () => {
+                    cy.visit(`/event/${slug}`);
 
-                        cy.visit(`/event/${slug}`);
+                    cy.wait(1000);
+                    cy.get('[data-cy=reg-btn]').click();
 
-                        cy.wait(1000);
-                        cy.get('[data-cy=reg-btn]').click();
-
-                        cy.get('li[class=chakra-toast]').contains('Påmeldingen din er registrert!');
-                    });
+                    cy.get('li[class=chakra-toast]').contains('Påmeldingen din er registrert!');
                 });
-            }
+            });
         }
     });
 });
