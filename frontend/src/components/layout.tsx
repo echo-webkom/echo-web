68630d4096e125a39bda975ef6e9b472a506687f
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-12-01 15:22:19 +0100

    🐛 Fix React Snowfall hydration error

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,4 +1,7 @@
 import { Box, Flex, Spacer } from '@chakra-ui/react';
+import { getMonth } from 'date-fns';
+import Snowfall from 'react-snowfall';
+import { useState, useEffect } from 'react';
 import Footer from '@components/footer';
 import Header from '@components/header';
 import AnimatedIcons from '@components/animated-icons';
@@ -9,8 +12,16 @@ interface Props {
 }
 
 const Layout = ({ children }: Props): JSX.Element => {
+    // https://nextjs.org/docs/messages/react-hydration-error
+    const [SSR, setSSR] = useState(true);
+
+    useEffect(() => {
+        setSSR(false);
+    }, []);
+
     return (
         <AnimatedIcons n={50}>
+            {!SSR && getMonth(new Date()) === 11 && <Snowfall snowflakeCount={200} color="#ffffff" />}
             <Flex direction="column" minH="100vh" data-testid="layout">
                 <Header />
                 <Box maxW="1500" w={['97%', null, null, '90%']} m="auto">
