076288c18f036f93d23204d1b09b37c424464a5c
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-12-01 17:00:19 +0100

    ✨ Add createdAt and modifedAt fields for User table
    Så vi kan se når brukere blir endret på, hvis vi vil filtrere vekk inaktive brukere f.eks.

--- backend/src/main/kotlin/no/uib/echo/schema/User.kt
+++ backend/src/main/kotlin/no/uib/echo/schema/User.kt
@@ -7,6 +7,9 @@ import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.jodatime.CurrentDateTime
+import org.jetbrains.exposed.sql.jodatime.datetime
+import org.joda.time.DateTime
 
 @Serializable
 data class UserJson(
@@ -24,6 +27,8 @@ object User : Table() {
     val alternateEmail: Column<String?> = text("alternate_email").nullable()
     val degreeYear: Column<Int?> = integer("degree_year").nullable()
     val degree: Column<String?> = text("degree").nullable()
+    val createdAt: Column<DateTime> = datetime("created_at").defaultExpression(CurrentDateTime)
+    val modifiedAt: Column<DateTime> = datetime("modified_at").defaultExpression(CurrentDateTime)
 
     override val primaryKey: PrimaryKey = PrimaryKey(email)
 }
