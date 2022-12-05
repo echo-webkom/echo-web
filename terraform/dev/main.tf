09ef64f5e6f37837c999c4c5c56df5a193a4b724
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-12-05 21:18:33 +0100

    🔧 Tweak Terraform & fix workflow

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -106,8 +106,9 @@ resource "azurerm_container_group" "echo_web_containers" {
     memory = 0.5
 
     environment_variables = {
-      "ENVIRONMENT"  = var.environment
-      "USE_JWT_TEST" = "true"
+      "MAX_POOL_SIZE" = 7
+      "ENVIRONMENT"   = var.environment
+      "USE_JWT_TEST"  = "true"
     }
 
     secure_environment_variables = {
