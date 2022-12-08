1c1eaa75108966261945439808cc46073c7ccce5
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-12-08 15:22:14 +0100

    🌐 Add english translation

--- frontend/src/pages/500.tsx
+++ frontend/src/pages/500.tsx
@@ -1,9 +1,11 @@
 import { Box, Center, Heading, Text } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import SEO from '@components/seo';
+import useLanguage from '@hooks/use-language';
 
 const ErrorPage = (): JSX.Element => {
     const router = useRouter();
+    const isNorwegian = useLanguage();
 
     return (
         <>
@@ -13,7 +15,9 @@ const ErrorPage = (): JSX.Element => {
                     <Heading py={['2rem', null, '3rem', '4rem']} fontSize="9rem">
                         500
                     </Heading>
-                    <Text fontSize="2rem">{router.query.msg ?? 'Det har skjedd en feil.'}</Text>
+                    <Text fontSize="2rem">
+                        {router.query.msg ?? (isNorwegian ? 'Det har skjedd en feil.' : 'An error has occurred')}
+                    </Text>
                 </Box>
             </Center>
         </>
