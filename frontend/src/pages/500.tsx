2530e237ec74d7354fa5c0a2d23720a2609b3a99
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-12-08 15:18:26 +0100

    ✨ Add custom error message for /500

--- frontend/src/pages/500.tsx
+++ frontend/src/pages/500.tsx
@@ -1,18 +1,23 @@
 import { Box, Center, Heading, Text } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import SEO from '@components/seo';
 
-const ErrorPage = (): JSX.Element => (
-    <>
-        <SEO title="Det har skjedd en feil" />
-        <Center>
-            <Box textAlign="center">
-                <Heading py="4rem" fontSize="9rem">
-                    500
-                </Heading>
-                <Text fontSize="2rem">Det har skjedd en feil.</Text>
-            </Box>
-        </Center>
-    </>
-);
+const ErrorPage = (): JSX.Element => {
+    const router = useRouter();
+
+    return (
+        <>
+            <SEO title="Det har skjedd en feil" />
+            <Center>
+                <Box textAlign="center">
+                    <Heading py={['2rem', null, '3rem', '4rem']} fontSize="9rem">
+                        500
+                    </Heading>
+                    <Text fontSize="2rem">{router.query.msg ?? 'Det har skjedd en feil.'}</Text>
+                </Box>
+            </Center>
+        </>
+    );
+};
 
 export default ErrorPage;
