cf5f66bf2cd101de8409250d2deeba53169da123
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-12-27 13:55:51 +0100

    🔧 Add testing dataset and change names

--- cms/sanity.config.ts
+++ cms/sanity.config.ts
@@ -4,7 +4,7 @@ import { visionTool } from '@sanity/vision';
 import { markdownSchema } from 'sanity-plugin-markdown';
 import { media } from 'sanity-plugin-media';
 import { colorInput } from '@sanity/color-input';
-import { RocketIcon, TerminalIcon } from '@sanity/icons';
+import { RocketIcon, RobotIcon, TerminalIcon } from '@sanity/icons';
 import { schemaTypes } from './schemas';
 
 const defaultConfig = {
@@ -17,7 +17,7 @@ export default defineConfig([
     {
         ...defaultConfig,
         name: 'production',
-        title: 'echo-web-prod',
+        title: 'echo web',
         icon: RocketIcon,
         basePath: '/prod',
         dataset: 'production',
@@ -25,9 +25,17 @@ export default defineConfig([
     {
         ...defaultConfig,
         name: 'develop',
-        title: 'echo-web-dev',
+        title: 'echo web – Utvikling',
         icon: TerminalIcon,
         basePath: '/dev',
         dataset: 'develop',
+    },
+    {
+        ...defaultConfig,
+        name: 'testing',
+        title: 'echo web – Testing',
+        icon: RobotIcon,
+        basePath: '/test',
+        dataset: 'testing',
     },
 ]);
