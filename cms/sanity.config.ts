0b0fd326ea6a9d2eaf3ad3ce252091ba8dfc5bb5
Andreas Bakseter <andreas_tkd@hotmail.com>
2022-12-27 02:48:18 +0100

    🔧 Add multiple workspaces to Desk

--- cms/sanity.config.ts
+++ cms/sanity.config.ts
@@ -6,13 +6,25 @@ import { media } from 'sanity-plugin-media';
 import { colorInput } from '@sanity/color-input';
 import { schemaTypes } from './schemas';
 
-export default defineConfig({
-    name: 'default',
-    title: 'echo-web',
-    projectId: 'pgq2pd26',
-    dataset: 'production',
+const defaultConfig = {
     plugins: [deskTool(), visionTool(), markdownSchema(), media(), colorInput()],
-    schema: {
-        types: schemaTypes,
+    schema: { types: schemaTypes },
+    projectId: 'pgq2pd26',
+};
+
+export default defineConfig([
+    {
+        ...defaultConfig,
+        name: 'production',
+        title: 'echo-web-prod',
+        basePath: '/prod',
+        dataset: 'production',
     },
-});
+    {
+        ...defaultConfig,
+        name: 'develop',
+        title: 'echo-web-dev',
+        basePath: '/dev',
+        dataset: 'develop',
+    },
+]);
