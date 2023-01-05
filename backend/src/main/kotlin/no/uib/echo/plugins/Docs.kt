6d7f22cdfbdc981ec759dc1d28a677d33d473ee0
Andreas Bakseter <andreas_tkd@hotmail.com>
2023-01-05 20:02:52 +0100

    🎨 ktlint

--- backend/src/main/kotlin/no/uib/echo/plugins/Docs.kt
+++ backend/src/main/kotlin/no/uib/echo/plugins/Docs.kt
@@ -1,9 +1,9 @@
 package no.uib.echo.plugins
 
 import io.ktor.server.application.Application
-import io.ktor.server.routing.routing
 import io.ktor.server.plugins.openapi.openAPI
 import io.ktor.server.plugins.swagger.swaggerUI
+import io.ktor.server.routing.routing
 
 fun Application.configureDocs() {
     routing {
