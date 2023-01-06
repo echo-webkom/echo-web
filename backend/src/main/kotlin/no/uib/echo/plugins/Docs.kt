67689c9dd2cf743cdf147bbba451e230d088d45e
Andreas Bakseter <andreas_tkd@hotmail.com>
2023-01-06 15:53:39 +0100

    🔥 Remove OpenAPI

--- backend/src/main/kotlin/no/uib/echo/plugins/Docs.kt
+++ backend/src/main/kotlin/no/uib/echo/plugins/Docs.kt
@@ -1,13 +1,11 @@
 package no.uib.echo.plugins
 
 import io.ktor.server.application.Application
-import io.ktor.server.plugins.openapi.openAPI
 import io.ktor.server.plugins.swagger.swaggerUI
 import io.ktor.server.routing.routing
 
 fun Application.configureDocs() {
     routing {
         swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml")
-        openAPI(path = "openapi", swaggerFile = "openapi/documentation.yaml")
     }
 }
