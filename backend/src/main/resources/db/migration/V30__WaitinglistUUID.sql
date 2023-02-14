f7f57fa1b04667ac176e8a4426b8c06caf1f79eb
felix <mrKaasa@protonmail.com>
2023-02-14 22:43:26 +0100

    fixing typo

--- backend/src/main/resources/db/migration/V30__WaitinglistUUID.sql
+++ backend/src/main/resources/db/migration/V30__WaitinglistUUID.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS waiting_list_uuid (
     uuid TEXT PRIMARY KEY,
     user_email TEXT NOT NULL,
     happening_slug TEXT NOT NULL,
-    "last_Notified" TIMESTAMP DEFAULT '1970-01-01 00:00:00.000000' NOT NULL,
+    "last_notified" TIMESTAMP DEFAULT '1970-01-01 00:00:00.000000' NOT NULL,
     CONSTRAINT fk_waiting_list_uuid_user_email__email FOREIGN KEY (user_email) REFERENCES "user"(email) ON DELETE RESTRICT ON UPDATE RESTRICT,
     CONSTRAINT fk_waiting_list_uuid_happening_slug__slug FOREIGN KEY (happening_slug) REFERENCES happening(slug) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
