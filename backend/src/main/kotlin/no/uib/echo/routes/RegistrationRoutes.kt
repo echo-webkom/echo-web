46972a32842ba1492604d05d7e91816fc781f371
Andreas Bakseter <andreas_tkd@hotmail.com>
2023-02-15 15:06:18 +0100

    🐛 Account for Bedkom regs in waitlist logic
    Fikser at folk kan melde seg på når det egt ikke er plass, pga. Bedkom-påmeldinger ikke blir telt med.

--- backend/src/main/kotlin/no/uib/echo/routes/RegistrationRoutes.kt
+++ backend/src/main/kotlin/no/uib/echo/routes/RegistrationRoutes.kt
@@ -283,7 +283,10 @@ fun Route.postRegistration(sendGridApiKey: String?, sendEmail: Boolean) {
                 true,
             )
 
-            val waitList = correctRange.spots in 1..countRegsInSpotRange || countRegsInSpotRangeWaitList > 0
+            val totalRegCount = countRegistrationsDegreeYear(registration.slug, 1..5, false)
+            val totalSpotsCount = spotRanges.sumOf { it.spots }
+
+            val waitList = correctRange.spots in 1..countRegsInSpotRange || countRegsInSpotRangeWaitList > 0 || totalRegCount >= totalSpotsCount
             val waitListSpot = countRegsInSpotRangeWaitList + 1
 
             val oldReg = transaction {
