3da702f8a779329dfcd1daeb9269f8bc8a2f0a39
Bo Aanes <boaanes@gmail.com>
2023-03-09 14:37:39 +0100

    Add warning message if backend is down
    also easter egg

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -1,11 +1,12 @@
-import { Box, Flex, Spacer } from '@chakra-ui/react';
+import { useSound } from 'use-sound';
+import { Alert, AlertIcon, Box, Center, Flex, Spacer } from '@chakra-ui/react';
 import { getMonth } from 'date-fns';
+import { useEffect, useState } from 'react';
 import Snowfall from 'react-snowfall';
-import { useState, useEffect } from 'react';
-import Footer from '@components/footer';
-import Header from '@components/header';
 import AnimatedIcons from '@components/animated-icons';
 import FeedbackButton from '@components/feedback-button';
+import Footer from '@components/footer';
+import Header from '@components/header';
 
 interface Props {
     children: React.ReactNode;
@@ -14,9 +15,22 @@ interface Props {
 const Layout = ({ children }: Props): JSX.Element => {
     // https://nextjs.org/docs/messages/react-hydration-error
     const [SSR, setSSR] = useState(true);
+    const [backendError, setBackendError] = useState(false);
+    const [play] = useSound('../tradeoffer.ogg');
 
     useEffect(() => {
         setSSR(false);
+
+        const pingBackend = async () => {
+            try {
+                const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost:8080'}/status`);
+                setBackendError(!res.ok);
+            } catch {
+                setBackendError(true);
+            }
+        };
+
+        void pingBackend();
     }, []);
 
     return (
@@ -25,6 +39,15 @@ const Layout = ({ children }: Props): JSX.Element => {
                 <Snowfall style={{ zIndex: -1 }} snowflakeCount={200} color="#ffffff" />
             )}
             <Flex direction="column" minH="100vh" data-testid="layout">
+                {backendError && (
+                    <Center>
+                        <Alert onMouseEnter={() => play()} borderRadius="0.5rem" m="1rem" maxW="600px" status="warning">
+                            <AlertIcon />
+                            Vi opplever for tiden problemer med backenden. Dette kan føre til at noen funksjoner ikke
+                            fungerer som de skal. Webkom jobber med å løse problemet så fort som mulig.
+                        </Alert>
+                    </Center>
+                )}
                 <Header />
                 <Box maxW="1500" w="full" px={['0.5rem', '1.5rem', null, '3rem']} mx="auto">
                     {children}
