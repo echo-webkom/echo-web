899eb81db90dfa6dfc85878d6d082129193ba082
Ole Magnus Fon Johnsen <me@omfj.no>
2023-03-14 17:27:09 +0100

    added profile happening preview

---
+++ frontend/src/components/profile-happening-preview.tsx
@@ -0,0 +1,114 @@
+import { Box, Flex, LinkBox, LinkOverlay, Spacer, Stack, Text, useColorModeValue } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import Image from 'next/image';
+import type { Happening } from '@api/happening';
+import { format, isToday, isPast } from 'date-fns';
+import { nb, enUS } from 'date-fns/locale';
+import { BiCalendar } from 'react-icons/bi';
+import useLanguage from '@hooks/use-language';
+
+interface Props {
+    isBedpres: boolean;
+    onWaitlist?: boolean;
+    event: Happening;
+}
+
+const ProfileHappeningPreview = ({ isBedpres, event, onWaitlist }: Props) => {
+    const isNorwegian = useLanguage();
+    const hoverColor = useColorModeValue('bg.light.hover', 'bg.dark.hover');
+    const logoUrl = event.logoUrl as string;
+    return (
+        <>
+            {isBedpres ? (
+                <LinkBox data-testid={event.slug}>
+                    <Flex alignItems="center" p={[0, null, null, null, 5]} _hover={{ bg: hoverColor }}>
+                        <Flex alignItems="center" gap="5">
+                            <Box display={['none', 'block']}>
+                                <Box pos="relative" overflow="hidden" borderRadius="50%" w="85px" h="85px">
+                                    <Image src={logoUrl} alt={event.title} fill />
+                                </Box>
+                            </Box>
+                            <Box>
+                                <Text fontWeight="regular" fontSize="larger">
+                                    <LinkOverlay as={NextLink} href={`/event/${event.slug}`}>
+                                        {event.title}
+                                    </LinkOverlay>
+                                </Text>
+                            </Box>
+                        </Flex>
+
+                        <Spacer />
+
+                        <Box>
+                            <Stack textAlign="right">
+                                <Flex alignItems="center" justifyContent="flex-end">
+                                    <BiCalendar />
+                                    {isToday(new Date(event.date)) ? (
+                                        <>
+                                            <Text ml="1" fontWeight="bold">
+                                                {isNorwegian ? `I dag ` : `Today `}
+                                            </Text>
+                                            <Text ml="1" fontSize="1rem">
+                                                {format(new Date(event.date), isNorwegian ? 'HH:mm' : 'h:aaa')}
+                                            </Text>
+                                        </>
+                                    ) : (
+                                        format(new Date(event.date), 'dd. MMM', { locale: isNorwegian ? nb : enUS })
+                                    )}
+                                </Flex>
+                                <Text fontSize={14}>
+                                    {isNorwegian
+                                        ? onWaitlist && <Text color={'red.200'}>På venteliste</Text>
+                                        : onWaitlist && <Text color={'red.200'}>On the waiting list</Text>}
+                                </Text>
+                            </Stack>
+                        </Box>
+                    </Flex>
+                </LinkBox>
+            ) : (
+                <LinkBox data-testid={event.slug}>
+                    <Flex alignItems="center">
+                        <Box>
+                            <LinkOverlay
+                                as={NextLink}
+                                href={`/event/${event.slug}`}
+                                _hover={{ textDecor: 'underline' }}
+                            >
+                                {event.title}
+                            </LinkOverlay>
+                        </Box>
+
+                        <Spacer />
+
+                        <Box>
+                            <Stack textAlign="right">
+                                <Flex alignItems="center" justifyContent="flex-end">
+                                    <BiCalendar />
+                                    {isToday(new Date(event.date)) ? (
+                                        <>
+                                            <Text ml="1" fontWeight="bold">
+                                                {isNorwegian ? `I dag ` : `Today `}
+                                            </Text>
+                                            <Text ml="1" fontSize="1rem">
+                                                {format(new Date(event.date), isNorwegian ? 'HH:mm' : 'h:aaa')}
+                                            </Text>
+                                        </>
+                                    ) : (
+                                        format(new Date(event.date), 'dd. MMM', { locale: isNorwegian ? nb : enUS })
+                                    )}
+                                </Flex>
+                                <Text fontSize={14}>
+                                    {isNorwegian
+                                        ? onWaitlist && <Text color={'red.200'}>På venteliste</Text>
+                                        : onWaitlist && <Text color={'red.200'}>On the waiting list</Text>}
+                                </Text>
+                            </Stack>
+                        </Box>
+                    </Flex>
+                </LinkBox>
+            )}
+        </>
+    );
+};
+
+export default ProfileHappeningPreview;
