15af7ec0db866adef8ec5119989f58db0fa88eea
Thea Kolnes <theakolnes@pc139-197.eduroam.uib.no>
2023-03-15 18:04:05 +0100

    added strikesJson

---
+++ backend/src/main/kotlin/no/uib/echo/schema/Strikes.kt
@@ -0,0 +1,25 @@
+package no.uib.echo.schema
+
+import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.jodatime.CurrentDateTime
+import org.jetbrains.exposed.sql.jodatime.datetime
+import org.joda.time.DateTime
+
+@Serializable
+data class StrikesJson(
+    val userEmail: String,
+    val reason: String,
+    val createdAt: String,
+    val modifiedAt: String
+)
+
+object Strikes : Table() {
+    val userEmail: Column<String> = text("user_email") references User.email
+    val reason: Column<String?> = text("reason").nullable()
+    val createdAt: Column<DateTime> = datetime("created_at").defaultExpression(CurrentDateTime)
+    val modifiedAt: Column<DateTime> = datetime("modified_at").defaultExpression(CurrentDateTime)
+
+    override val primaryKey: PrimaryKey = PrimaryKey(userEmail)
+}
