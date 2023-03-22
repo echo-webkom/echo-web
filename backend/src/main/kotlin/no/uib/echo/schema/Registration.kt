b70aa313765867ebc69d942c7410c2e91c85de61
nikolaus <Nikolaus.Engh@student.uib.no>
2023-03-22 17:45:43 +0100

    fikset ting fra forrige PR

--- backend/src/main/kotlin/no/uib/echo/schema/Registration.kt
+++ backend/src/main/kotlin/no/uib/echo/schema/Registration.kt
@@ -60,9 +60,8 @@ object Registration : Table() {
 fun countRegistrationsDegreeYear(
     slug: String,
     range: IntRange,
-    status: Status,
+    status: Status
 ): Int {
-
     return transaction {
         Registration.select {
             Registration.happeningSlug eq slug and
