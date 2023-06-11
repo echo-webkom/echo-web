f1b3d860da951af3dc59959d3cf32b5c6d6df104
Andreas Bakseter <andreas_tkd@hotmail.com>
2023-06-11 14:42:17 +0200

    ⬆️ Also upgrade other Kotlin packages

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -14,8 +14,8 @@ project.setProperty("mainClassName", "no.uib.echo.ApplicationKt")
 
 plugins {
     application
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.serialization") version "1.8.21"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.serialization") version "1.8.22"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
     id("com.adarshr.test-logger") version "3.2.0"
