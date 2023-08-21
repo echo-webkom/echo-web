16c617e73fb4d194b2c00f6a46e088d46e1f9de9
Ole Magnus Fon Johnsen <me@omfj.no>
2023-08-21 17:55:28 +0200

    ✏️ Check valid year degree combination

---
+++ backend/src/main/kotlin/no/uib/echo/lib/UserValidation.kt
@@ -0,0 +1,15 @@
+package no.uib.echo.lib
+
+import no.uib.echo.schema.Degree
+import no.uib.echo.schema.bachelors
+import no.uib.echo.schema.masters
+
+object UserValidation {
+    fun validateDegreeAndDegreeYear(degree: Degree?, degreeYear: Int?): Boolean {
+        if (degreeYear == null || degree == null) {
+            return false
+        }
+
+        return (degree in bachelors && degreeYear in 1..3) || (degree in masters && degreeYear in 4..5) || (degree == Degree.ARMNINF && degreeYear == 1)
+    }
+}
