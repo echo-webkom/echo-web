babe9dd31aca70031b3d36ddb55171c392e0593b
Ole Magnus Fon Johnsen <me@omfj.no>
2023-10-07 19:34:22 +0200

    ✨ Add brochure

---
+++ frontend/src/pages/brosjyre.tsx
@@ -0,0 +1,13 @@
+const BrochurePage = () => {
+    return (
+        <iframe
+            style={{
+                width: '100%',
+                height: '800px',
+            }}
+            src="https://www.visbrosjyre.no/echo/WebView/"
+        ></iframe>
+    );
+};
+
+export default BrochurePage;
